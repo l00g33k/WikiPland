0168f0beee09fa35695ab846d982d04271c0ff6b
default_git_name <default_git_email>
2013-12-16 15:19:04 +0000

    perio log battery voltages and plot graphs

---
+++ l00http_periobattery.pl
@@ -0,0 +1,191 @@
+use strict;
+use warnings;
+
+# Release under GPLv2 or later version by l00g33k@gmail.com, 2010/02/14
+
+
+my ($lastcalled, $battlog, $battcnt, $battpolls, $perltime);
+my %config = (proc => "l00http_periobattery_proc",
+              desc => "l00http_periobattery_desc",
+              perio => "l00http_periobattery_perio");
+my ($savedpath, $battperc, $battvolts, $batttemp, $battmA, $lasttimestamp);
+my $interval = 0, $lastcalled = 0;
+$battcnt = 0;
+$perltime = 0;
+$savedpath = '';
+$battlog = '';
+$battpolls = 0;
+$lasttimestamp = 0;
+$battperc = 0;
+$battvolts = 0;
+$batttemp = 0;
+$battmA = 0;
+
+sub l00http_periobattery_desc {
+    my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
+    # Descriptions to be displayed in the list of modules table
+    # at http://localhost:20337/
+    " C: periobattery: Periodic logging of battery by dmesg";
+}
+
+
+sub l00http_periobattery_proc {
+    my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
+    my $sock = $ctrl->{'sock'};     # dereference network socket
+    my $form = $ctrl->{'FORM'};     # dereference FORM data
+    my ($tmp);
+ 
+    # get submitted name and print greeting
+    if (defined ($form->{"interval"}) && ($form->{"interval"} >= 0)) {
+        $interval = $form->{"interval"};
+    }
+    if (defined ($form->{"stop"})) {
+        $interval = 0;
+    }
+    if (defined ($form->{"clear"})) {
+        $battcnt = 0;
+        $battlog = '';
+        $battpolls = 0;
+        $savedpath = '';
+    }
+    # save path
+    if (defined ($form->{"save"}) && defined ($form->{'savepath'}) && (length ($form->{'savepath'}) > 0)) {
+        if (open (OU, ">$form->{'savepath'}")) {
+            print OU $battlog;
+            close (OU);
+            $savedpath = $form->{'savepath'};
+        }
+    }
+    if (defined ($form->{"overwrite"}) && defined ($form->{'owpath'}) && (length ($form->{'owpath'}) > 0)) {
+        if (open (OU, ">$form->{'owpath'}")) {
+            print OU $battlog;
+            close (OU);
+            $savedpath = $form->{'owpath'};
+        }
+    }
+
+
+    # Send HTTP and HTML headers
+    print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . $ctrl->{'htmlttl'} . $ctrl->{'htmlhead2'};
+    print $sock "<a name=\"top\"></a>\n";
+    print $sock "$ctrl->{'home'} <a href=\"$ctrl->{'quick'}\">QUICK</a> <a href=\"/periobattery.htm\">Refresh</a><p> \n";
+
+    print $sock "${battperc}% ${battvolts}V ${batttemp}C ${battmA}mA. <a href=\"#end\">end</a>\n";
+
+    print $sock "<form action=\"/periobattery.htm\" method=\"get\">\n";
+    print $sock "<table border=\"1\" cellpadding=\"5\" cellspacing=\"3\">\n";
+
+    print $sock "    <tr>\n";
+    print $sock "        <td>Run interval (sec, e.g. 30):</td>\n";
+    print $sock "        <td><input type=\"text\" size=\"6\" name=\"interval\" value=\"$interval\"></td>\n";
+    print $sock "    </tr>\n";
+                                                
+    print $sock "    <tr>\n";
+    print $sock "        <td><input type=\"submit\" name=\"submit\" value=\"Submit\"> \n";
+    print $sock "         <input type=\"submit\" name=\"stop\" value=\"Stop\"></td>\n";
+    print $sock "        <td><input type=\"submit\" name=\"clear\" value=\"Clear\"></td>\n";
+    print $sock "    </tr>\n";
+
+    print $sock "</table>\n";
+    print $sock "</form>\n";
+
+    print $sock "<form action=\"/periobattery.htm\" method=\"get\">\n";
+    print $sock "<table border=\"1\" cellpadding=\"5\" cellspacing=\"3\">\n";
+
+    print $sock "    <tr>\n";
+    print $sock "        <td><input type=\"submit\" name=\"save\" value=\"Save new\"></td>\n";
+    $tmp = "$ctrl->{'workdir'}del/$ctrl->{'now_string'}_battery.csv";
+    $tmp =~ s/ /_/g;
+    print $sock "        <td><input type=\"text\" size=\"16\" name=\"savepath\" value=\"$tmp\"></td>\n";
+    print $sock "    </tr>\n";
+                                                
+    print $sock "    <tr>\n";
+    print $sock "        <td><input type=\"submit\" name=\"overwrite\" value=\"Overwrite\"></td>\n";
+    print $sock "        <td><input type=\"text\" size=\"16\" name=\"owpath\" value=\"$savedpath\"></td>\n";
+    print $sock "    </tr>\n";
+                                                
+    print $sock "</table>\n";
+    print $sock "</form>\n";
+
+    if (length ($savedpath) > 5) {
+        print $sock "Launcher to last saved: <a href=\"/launcher.htm?path=$savedpath\">$savedpath</a><p>\n";
+    }
+
+    print $sock "<pre>\n";
+    $tmp = 0;
+    foreach $_ (split("\n", $battlog)) {
+        $tmp++;
+        if ($tmp < 100) {
+            printf $sock ("%3d: $_\n", $tmp);
+        } elsif ($tmp == 100) {
+            printf $sock ("%3d: $_\n", $tmp);
+            print $sock "\nskipping ".($battcnt - 100 * 2)." lines\n\n";
+        } elsif ($tmp > ($battcnt - 100)) {
+            printf $sock ("%3d: $_\n", $tmp);
+        }
+    }
+    print $sock "</pre>\n";
+    print $sock "<p><a href=\"#top\">Jump to top</a><p>\n";
+    print $sock "<p>Lines: $battcnt. Polls: $battpolls\n";
+    print $sock "<a name=\"end\"></a>\n";
+
+    # send HTML footer and ends
+    print $sock $ctrl->{'htmlfoot'};
+}
+
+sub l00http_periobattery_perio {
+    my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
+    my ($tempe, $bstat);
+    my ($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp);
+
+    if (($interval > 0) && 
+        (($lastcalled == 0) || (time >= ($lastcalled + $interval)))) {
+        $lastcalled = time;
+
+        $tempe = '';
+        if ($ctrl->{'os'} eq 'and') {
+            # This SL4A call doesn't work for my Slide:(
+            #$tmp = $ctrl->{'droid'}->batteryGetLevel();
+            #print $sock "batteryGetLevel $tmp\n";
+            #&l00httpd::dumphash ("tmp", $tmp);
+            #$tmp = $ctrl->{'droid'}->batteryGetStatus();
+            #print $sock "batteryGetStatus $tmp\n";
+            #&l00httpd::dumphash ("tmp", $tmp);
+
+            # On Slide, dmesg contains battery status:
+            #<6>[ 7765.397493] [BATT] ID=2, level=89, vol=4209, temp=326, curr=-214, dis_curr=0, chg_src=1, chg_en=1, over_vchg=0, batt_state=1 at 7765326083644 (2013-12-11 12:08:08.239292486 UTC)
+            $bstat = 'Only my Slide, dmesg contains a line with battery level: [BATT] ID=2, level=89, vol=4209, temp=326... If you see this line, either dmesg did not work, or the line format is different. Contact me to support it.';
+            foreach $_ (split("\n", `dmesg`)) {
+                if (/\[BATT\] ID=2/) {
+                    $bstat = $_;
+                }
+            }
+            if (($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp) 
+                 = $bstat =~ /level=(\d+), vol=(\d+), temp=(\d+), curr=(-*\d+), dis_curr=(\d+), chg_src=(\d+), chg_en=(\d+), over_vchg=(\d+), batt_state=(\d+) at \d+ \((.+? UTC)\)/) {
+                if ($lasttimestamp ne $timestamp) {
+                    $lasttimestamp = $timestamp;
+                    $battperc = $level;
+                    $battvolts = $vol / 1000;
+                    $batttemp = $temp / 10;
+                    $battmA = $curr + $dis_curr;
+                    $tempe = "$ctrl->{'now_string'}: $bstat\n";
+                    $battcnt++;
+                }
+            }
+        }
+        if ($perltime != 0) {
+            # subsequently
+            $battlog .= $tempe;
+        } else {
+            # first time
+            $battlog = $tempe;
+        }
+        $perltime = time;
+        $battpolls++;
+    }
+
+    $interval;
+}
+
+
+\%config;
---
+++ l00http_rptbattery.pl
@@ -0,0 +1,130 @@
+use strict;
+use warnings;
+use l00backup;
+
+# Release under GPLv2 or later version by l00g33k@gmail.com, 2010/02/14
+
+# this is a simple template, a good starting point to make your own modules
+
+my %config = (proc => "l00http_rptbattery_proc",
+              desc => "l00http_rptbattery_desc");
+my ($buffer, $lastbuf, $timeslot);
+$lastbuf = '';
+$timeslot = 60 * 1;
+
+sub l00http_rptbattery_desc {
+    my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
+    # Descriptions to be displayed in the list of modules table
+    # at http://localhost:20337/
+    "rptbattery: periobattery.pl reporter";
+}
+
+sub l00http_rptbattery_proc {
+    my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
+    my $sock = $ctrl->{'sock'};     # dereference network socket
+    my $form = $ctrl->{'FORM'};     # dereference FORM data
+    my ($path, $fname, $tmp, $output, $table);
+    my ($lnno, $svgperc, $svgvolt, $svgtemp, $svgmA, $battcnt);
+    my ($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp);
+    my ($yr, $mo, $da, $hr, $mi, $se, $now);
+
+
+    if (defined ($form->{'path'})) {
+        $path = $form->{'path'};
+        ($fname) = $path =~ /[\\\/]([^\\\/]+)$/;
+    } else {
+        $path = '(none)';
+        $fname = '(none)';
+    }
+
+    # Send HTTP and HTML headers
+    print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>$fname rptbattery</title>" .$ctrl->{'htmlhead2'};
+    print $sock "<a name=\"top\"></a>$ctrl->{'home'} <a href=\"$ctrl->{'quick'}\">Quick</a> <a href=\"#end\">Jump to end</a><br>\n";
+    if (defined ($form->{'path'})) {
+        $tmp = $path;
+        if ($ctrl->{'os'} eq 'win') {
+            $tmp =~ s/\//\\/g;
+        }
+        print $sock "<a href=\"/clip.htm?update=Copy+to+clipboard&clip=$tmp\">Path</a>: ";
+        print $sock "<a href=\"/view.htm?path=$form->{'path'}\">$form->{'path'}</a><br>\n";
+    }
+
+
+    # get submitted name and print greeting
+    if (open (IN, "<$form->{'path'}")) {
+        $svgperc = '';
+        $svgvolt = '';
+        $svgtemp = '';
+        $svgmA = '';
+        $lnno = 0;
+        $output = "<pre>\n";
+        $table = '';
+        while (<IN>) {
+            s/\r//;
+            s/\n//;
+            $output .= "$_\n";
+
+            if (($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp) 
+                = /level=(\d+), vol=(\d+), temp=(\d+), curr=(-*\d+), dis_curr=(\d+), chg_src=(\d+), chg_en=(\d+), over_vchg=(\d+), batt_state=(\d+) at \d+ \((.+? UTC)\)/) {
+                $vol /= 1000;
+                $temp /= 10;
+                if (($yr, $mo, $da, $hr, $mi, $se) = $timestamp =~ /^(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)\./) {
+                    # 2013-12-16 14:08:37.807146248 UTC
+                    # convert to seconds
+                    $yr -= 1900;
+                    $mo--;
+                    $now = &l00mktime::mktime ($yr, $mo, $da, $hr, $mi, $se);
+                    $svgperc .= "$now,$level ";
+                    $svgvolt .= "$now,$vol ";
+                    $svgtemp .= "$now,$temp ";
+                    $tmp = $curr + $dis_curr;
+                    $svgmA .= "$now,$tmp ";
+                    $lnno++;
+
+                    $chg_src =~ s/0/0 (off)/;
+                    $chg_src =~ s/1/1 (usb)/;
+                    $chg_src =~ s/2/2 (wall)/;
+                    $chg_en =~ s/0/0 (off)/;
+                    $chg_en =~ s/1/1 (usb)/;
+                    $chg_en =~ s/2/2 (wall)/;
+                    $table = "||$lnno||$level||$vol||$temp||$curr||$dis_curr||$chg_src||$chg_en||$over_vchg||$batt_state||$timestamp||\n" . $table;
+                }
+            }
+        }
+        $output .= "</pre>\n";
+        $table = "||#||level||vol||temp||curr||dis curr||chg src||chg en||over vchg||batt state||time stamp||\n" . $table;
+        close (IN);
+
+        if ($lnno > 1) {
+            if ($svgperc ne '') {
+                &l00svg::plotsvg ('battpercentage', $svgperc, 500, 300);
+                print $sock "<p>Level %:<br><a href=\"/svg.pl?graph=battpercentage&view=\"><img src=\"/svg.pl?graph=battpercentage\" alt=\"alt\"></a>\n";
+            }
+            if ($svgvolt ne '') {
+                &l00svg::plotsvg ('battvolt', $svgvolt, 500, 300);
+                print $sock "<p>Volts:<br><a href=\"/svg.pl?graph=battvolt&view=\"><img src=\"/svg.pl?graph=battvolt\" alt=\"alt\"></a>\n";
+            }
+            if ($svgtemp ne '') {
+                &l00svg::plotsvg ('batttemp', $svgtemp, 500, 300);
+                print $sock "<p>Temp:<br><a href=\"/svg.pl?graph=batttemp&view=\"><img src=\"/svg.pl?graph=batttemp\" alt=\"alt\"></a>\n";
+            }
+            if ($svgmA ne '') {
+                &l00svg::plotsvg ('battmA', $svgmA, 500, 300);
+                print $sock "<p>mA:<br><a href=\"/svg.pl?graph=battmA&view=\"><img src=\"/svg.pl?graph=battmA\" alt=\"alt\"></a>\n";
+            }
+        }
+    }
+
+    print $sock "<p>\n";
+    $table =~ s/(\.\d)\d+ UTC/ UTC/g;
+    print $sock &l00wikihtml::wikihtml ($ctrl, "", $table, 0);
+
+    print $sock "$output<hr><a href=\"#top\">Jump to top</a>,\n";
+    print $sock "<p><a name=\"end\">end</a>\n";
+
+    # send HTML footer and ends
+    print $sock $ctrl->{'htmlfoot'};
+}
+
+
+\%config;
