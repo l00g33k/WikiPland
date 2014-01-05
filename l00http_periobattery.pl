1ddb79a60aeedec020b6ecbda9fde55f7e338c40
default_git_name <default_git_email>
2014-01-05 06:13:49 +0000

    crypt: send to/from ram. periobattery: plot last $len

--- l00http_periobattery.pl
+++ l00http_periobattery.pl
@@ -9,7 +9,8 @@ my %config = (proc => "l00http_periobattery_proc",
               desc => "l00http_periobattery_desc",
               perio => "l00http_periobattery_perio");
 my ($savedpath, $battperc, $battvolts, $batttemp, $battmA, $lasttimestamp);
-my ($table, $tablehdr, $firstdmesg, $lastdmesg);
+my ($table, $tablehdr, $firstdmesg, $lastdmesg, $maxreclen, $skip, $len);
+my ($graphwd, $graphht);
 my $interval = 0, $lastcalled = 0;
 $battcnt = 0;
 $perltime = 0;
@@ -25,6 +26,12 @@ $table = '';
 $tablehdr = "||#||level||vol||C||curr||chg src||chg en||over vchg||batt state||time stamp||\n";
 $firstdmesg = '';
 $lastdmesg = '';
+$maxreclen = 60 * 24 * 7;
+$skip = -1;
+$len = 60 * 6;
+$graphwd = 500;
+$graphht = 300;
+
 
 sub l00http_periobattery_suspend {
     my ($ctrl) = @_;
@@ -33,8 +40,8 @@ sub l00http_periobattery_suspend {
     # suspend to sdcard so it can be resumed after restart
     &l00httpd::l00fwriteOpen($ctrl, "$ctrl->{'workdir'}tmp/l00_periobattery_vals.saved");
     &l00httpd::l00fwriteBuf($ctrl, "interval=$interval\n");
-    #&l00httpd::l00fwriteBuf($ctrl, "battcnt=$battcnt\n");
-    #&l00httpd::l00fwriteBuf($ctrl, "battpolls=$battpolls\n");
+    &l00httpd::l00fwriteBuf($ctrl, "battcnt=$battcnt\n");
+    &l00httpd::l00fwriteBuf($ctrl, "maxreclen=$maxreclen\n");
     &l00httpd::l00fwriteBuf($ctrl, "savedpath=$savedpath\n");
     &l00httpd::l00fwriteBuf($ctrl, "perltime=$perltime\n");
     &l00httpd::l00fwriteBuf($ctrl, "firstdmesg=$firstdmesg");
@@ -57,8 +64,8 @@ sub l00http_periobattery_suspend {
 
     l00httpd::dbp($config{'desc'}, "Suspend to sdcard:\n");
     l00httpd::dbp($config{'desc'}, "interval=$interval\n");
-    #l00httpd::dbp($config{'desc'}, "battcnt=$battcnt\n");
-    #l00httpd::dbp($config{'desc'}, "battpolls=$battpolls\n");
+    l00httpd::dbp($config{'desc'}, "battcnt=$battcnt\n");
+    l00httpd::dbp($config{'desc'}, "maxreclen=$maxreclen\n");
     l00httpd::dbp($config{'desc'}, "savedpath=$savedpath\n");
     l00httpd::dbp($config{'desc'}, "perltime=$perltime\n");
     l00httpd::dbp($config{'desc'}, "firstdmesg=$firstdmesg");
@@ -84,21 +91,20 @@ sub l00http_periobattery_resume {
         $firstdmesg = '';
         $lastdmesg = '';
         $table = '';
+        $maxreclen = 60 * 24 * 7;
 
         while ($_ = &l00httpd::l00freadLine($ctrl)) {
             if (/interval=(\d+)/) {
                 $interval = $1;
             }
-#           if (/battcnt=(\d+)/) {
-#               $battcnt = $1;
-#           }
-#           if (/battpolls=(\d+)/) {
-#               $battpolls = $1;
-#           }
+            if (/battcnt=(\d+)/) {
+                $battcnt = $1;
+            }
+            if (/maxreclen=(\d+)/) {
+                $maxreclen = $1;
+            }
             if (/savedpath=(.+)/) {
                 $savedpath = $1;
-            } else {
-                $savedpath = '';
             }
 
             if (/perltime=(\d+)/) {
@@ -122,11 +128,7 @@ sub l00http_periobattery_resume {
         $table = &l00httpd::l00freadAll($ctrl);
         if (!defined($table)) {
             $table = '';
-        } else {
             $battcnt = 0;
-            foreach $_ (split("\n", $table)) {
-                $battcnt++;
-            }
         }
 
         l00httpd::dbp($config{'desc'}, "battlog:\n");
@@ -136,7 +138,7 @@ sub l00http_periobattery_resume {
         l00httpd::dbp($config{'desc'}, "Resumed from sdcard:\n");
         l00httpd::dbp($config{'desc'}, "interval=$interval\n");
         l00httpd::dbp($config{'desc'}, "battcnt=$battcnt\n");
-        #l00httpd::dbp($config{'desc'}, "battpolls=$battpolls\n");
+        l00httpd::dbp($config{'desc'}, "maxreclen=$maxreclen\n");
         l00httpd::dbp($config{'desc'}, "savedpath=$savedpath\n");
         l00httpd::dbp($config{'desc'}, "perltime=$perltime\n");
         l00httpd::dbp($config{'desc'}, "firstdmesg=$firstdmesg");
@@ -172,7 +174,10 @@ sub l00http_periobattery_proc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     my $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
-    my ($tmp, $lnno, $keep);
+    my ($tmp, $lnno, $keep, $noln);
+    my ($svgperc, $svgvolt, $svgtemp, $svgmA);
+    my ($yr, $mo, $da, $hr, $mi, $se, $now);
+    my ($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp);
  
     # get submitted name and print greeting
     if (defined ($form->{"interval"}) && ($form->{"interval"} >= 0)) {
@@ -200,21 +205,42 @@ sub l00http_periobattery_proc {
         &l00httpd::l00fwriteBuf($ctrl, $tmp);
         &l00httpd::l00fwriteClose($ctrl);
     }
+    if ((defined ($form->{'skip'})) && ($form->{'skip'} =~ /([0-9\-]+)/)) {
+        $skip = $1;
+    }
+    if ((defined ($form->{'len'})) && ($form->{'len'} =~ /(\d+)/)) {
+        $len = $1;
+    }
+    if ((defined ($form->{'graphwd'})) && ($form->{'graphwd'} =~ /(\d+)/)) {
+        $graphwd = $1;
+    }
+    if ((defined ($form->{'graphht'})) && ($form->{'graphht'} =~ /(\d+)/)) {
+        $graphht = $1;
+    }
     if (defined ($form->{"suspend"})) {
         &l00http_periobattery_suspend($ctrl);
     }
     if (defined ($form->{"resume"})) {
         &l00http_periobattery_resume($ctrl);
     }
+    if ((defined ($form->{"maxreclen"})) && ($form->{"maxreclen"} =~ /(\d+)/)) {
+        $maxreclen = $1;
+        # trim if necessary
+        $noln = $table =~ s/\n/\n/g;
+        print "periobattery: trimming ", $noln - $maxreclen, " lines\n";
+        while ($noln > $maxreclen) {
+            $table   =~ s/\n.*?$//;   # trim from the end, oldest
+            $battlog =~ s/^.*?\n//;   # trim from the start, oldest
+            $noln = $table =~ s/\n/\n/g;
+        }
+    }
     if ((defined ($form->{"keep"})) && ($form->{"keep"} =~ /(\d+)/)) {
         $keep = $1;
         $tmp = '';
         $lnno = 0;
+        $noln = $table =~ s/\n/\n/g;
         foreach $_ (split("\n", $table)) {
-            if ($lnno == 0) {
-                $tmp = "$_\n";
-            }
-            if ($lnno > ($battcnt - $keep)) {
+            if ($lnno > ($noln - $keep)) {
                 $tmp .= "$_\n";
             }
             $lnno++;
@@ -224,17 +250,12 @@ sub l00http_periobattery_proc {
         $tmp = '';
         $lnno = 0;
         foreach $_ (split("\n", $battlog)) {
-            if ($lnno == 0) {
-                $tmp = "$_\n";
-            }
-            if ($lnno > ($battcnt - $keep)) {
+            if ($lnno > ($noln - $keep)) {
                 $tmp .= "$_\n";
             }
             $lnno++;
         }
         $battlog = $tmp;
-
-        $battcnt = $keep;
     }
     if (defined ($form->{"clear"})) {
         $interval = 0;
@@ -266,9 +287,64 @@ sub l00http_periobattery_proc {
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . $ctrl->{'htmlttl'} . $ctrl->{'htmlhead2'};
     print $sock "<a name=\"top\"></a>\n";
-    print $sock "$ctrl->{'home'} <a href=\"$ctrl->{'quick'}\">QUICK</a> <a href=\"/periobattery.htm\">Refresh</a><br>\n";
+    print $sock "$ctrl->{'home'} <a href=\"$ctrl->{'quick'}\">QUICK</a> <a href=\"/periobattery.htm\">Refresh</a> ";
+    print $sock "<a href=\"/periobattery.htm?graphs=\">graphs</a><br>\n";
 
     print $sock "${battperc}% ${battvolts}V ${batttemp}C ${battmA}mA. ($battcnt) <a href=\"#end\">end</a>\n";
+
+
+    if (defined ($form->{"graphs"})) {
+        $svgperc = '';
+        $svgvolt = '';
+        $svgtemp = '';
+        $svgmA = '';
+        $lnno = 0;
+        $noln = $battlog =~ s/\n/\n/g;
+        foreach $_ (split("\n", $battlog)) {
+            if (($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp) 
+                = /level=(\d+), vol=(\d+), temp=(\d+), curr=(-*\d+), dis_curr=(\d+), chg_src=(\d+), chg_en=(\d+), over_vchg=(\d+), batt_state=(\d+) at \d+ \((.+? UTC)\)/) {
+                $vol /= 1000;
+                $temp /= 10;
+                if (($yr, $mo, $da, $hr, $mi, $se) = $timestamp =~ /^(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)\./) {
+                    # 2013-12-16 14:08:37.807146248 UTC
+                    # convert to seconds
+                    $yr -= 1900;
+                    $mo--;
+                    $now = &l00mktime::mktime ($yr, $mo, $da, $hr, $mi, $se);
+                    $lnno++;
+                    if ((($skip >= 0) && ($lnno >= $skip) && ($lnno <= $skip + $len))  ||
+					    # $skip >= 0: skip $skip and plot $len
+                        (($skip <  0) && ($lnno > ($noln - $len)))) {
+						# $skip < 0: plot last $len
+					    # within skip and len (poor man's zoom in)
+                        $svgperc .= "$now,$level ";
+                        $svgvolt .= "$now,$vol ";
+                        $svgtemp .= "$now,$temp ";
+                        $tmp = $curr + $dis_curr;
+                        $svgmA .= "$now,$tmp ";
+                    }
+                }
+            }
+        }
+        if ($svgvolt ne '') {
+            &l00svg::plotsvg ('battvolt', $svgvolt, $graphwd, $graphht);
+            $timestamp =~ s/(\.\d)\d+ UTC/ UTC/g;
+            print $sock "<p>$vol V $temp C $tmp mA $timestamp\n";
+            print $sock "<p>Volts:<br><a href=\"/svg.htm?graph=battvolt&view=\"><img src=\"/svg.htm?graph=battvolt\" alt=\"voltage over time\"></a>\n";
+        }
+        if ($svgperc ne '') {
+            &l00svg::plotsvg ('battpercentage', $svgperc, $graphwd, $graphht);
+            print $sock "<p>Level %:<br><a href=\"/svg.htm?graph=battpercentage&view=\"><img src=\"/svg.htm?graph=battpercentage\" alt=\"level % over time\"></a>\n";
+        }
+        if ($svgmA ne '') {
+            &l00svg::plotsvg ('battmA', $svgmA, $graphwd, $graphht);
+            print $sock "<p>mA:<br><a href=\"/svg.htm?graph=battmA&view=\"><img src=\"/svg.htm?graph=battmA\" alt=\"charge/discharge current over time\"></a>\n";
+        }
+        if ($svgtemp ne '') {
+            &l00svg::plotsvg ('batttemp', $svgtemp, $graphwd, $graphht);
+            print $sock "<p>Temp:<br><a href=\"/svg.htm?graph=batttemp&view=\"><img src=\"/svg.htm?graph=batttemp\" alt=\"temperature over time\"></a>\n";
+        }
+    }
 
     print $sock "<form action=\"/periobattery.htm\" method=\"get\">\n";
     print $sock "<table border=\"1\" cellpadding=\"3\" cellspacing=\"0\">\n";
@@ -276,6 +352,11 @@ sub l00http_periobattery_proc {
     print $sock "    <tr>\n";
     print $sock "        <td>Run interval (sec, e.g. 30):</td>\n";
     print $sock "        <td><input type=\"text\" size=\"6\" name=\"interval\" value=\"$interval\"></td>\n";
+    print $sock "    </tr>\n";
+                                                
+    print $sock "    <tr>\n";
+    print $sock "        <td>Max record length:</td>\n";
+    print $sock "        <td><input type=\"text\" size=\"6\" name=\"maxreclen\" value=\"$maxreclen\"></td>\n";
     print $sock "    </tr>\n";
                                                 
     print $sock "    <tr>\n";
@@ -324,14 +405,15 @@ sub l00http_periobattery_proc {
     # print table
     $tmp = $tablehdr;
     $lnno = 0;
+    $noln = $table =~ s/\n/\n/g;
     foreach $_ (split("\n", $table)) {
         $lnno++;
         if ($lnno < 60) {
             $tmp .= "$_\n";
         } elsif ($lnno == 60) {
-            $tmp .= "\nskipping ".($battcnt - 60 * 2)." lines\n";
+            $tmp .= "\nskipping ".($noln - 60 * 2)." lines\n";
             $tmp .= $tablehdr;
-        } elsif ($lnno > ($battcnt - 60)) {
+        } elsif ($lnno > ($noln - 60)) {
             $tmp .= "$_\n";
         }
     }
@@ -347,8 +429,8 @@ sub l00http_periobattery_proc {
             printf $sock ("%3d: $_\n", $tmp);
         } elsif ($tmp == 60) {
             printf $sock ("%3d: $_\n", $tmp);
-            print $sock "\nskipping ".($battcnt - 60 * 2)." lines\n\n";
-        } elsif ($tmp > ($battcnt - 60)) {
+            print $sock "\nskipping ".($noln - 60 * 2)." lines\n\n";
+        } elsif ($tmp > ($noln - 60)) {
             printf $sock ("%3d: $_\n", $tmp);
         }
     }
@@ -372,14 +454,33 @@ sub l00http_periobattery_proc {
 
     print $sock "    <tr>\n";
     print $sock "        <td><input type=\"submit\" name=\"trim\" value=\"Trim\"></td>\n";
-    print $sock "        <td>and keep last <input type=\"text\" size=\"6\" name=\"keep\" value=\"$battcnt\"></td>\n";
+    print $sock "        <td>and keep last <input type=\"text\" size=\"6\" name=\"keep\" value=\"$noln\"></td>\n";
+    print $sock "    </tr>\n";
+
+    print $sock "</table>\n";
+    print $sock "</form>\n";
+
+    print $sock "<br><form action=\"/periobattery.htm\" method=\"get\">\n";
+    print $sock "<table border=\"1\" cellpadding=\"3\" cellspacing=\"0\">\n";
+
+    print $sock "    <tr>\n";
+    print $sock "        <td><input type=\"submit\" name=\"graphsz\" value=\"Set\"> \n";
+    print $sock "        <td>Graph width: <input type=\"text\" size=\"6\" name=\"graphwd\" value=\"$graphwd\">\n";
+    print $sock "                 height: <input type=\"text\" size=\"6\" name=\"graphht\" value=\"$graphht\"></td>\n";
+    print $sock "    </tr>\n";
+
+    print $sock "    <tr>\n";
+    print $sock "        <td>Skip</td>\n";
+    print $sock "        <td>first <input type=\"text\" size=\"6\" name=\"skip\" value=\"$skip\"> points and use up to \n";
+    print $sock "        <input type=\"text\" size=\"6\" name=\"len\" value=\"$len\">\n";
+    print $sock "        samples. Skip -1 to use record tail.</td>\n";
     print $sock "    </tr>\n";
 
     print $sock "</table>\n";
     print $sock "</form>\n";
 
 
-    print $sock "<p>Lines: $battcnt. Polls: $battpolls\n";
+    print $sock "<p>Lines: $noln. Polls: $battpolls\n";
     print $sock "<a name=\"end\"></a>\n";
 
     # send HTML footer and ends
@@ -388,7 +489,7 @@ sub l00http_periobattery_proc {
 
 sub l00http_periobattery_perio {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
-    my ($tempe, $bstat);
+    my ($tempe, $bstat, $noln);
     my ($level, $vol, $temp, $curr, $dis_curr, $chg_src, $chg_en, $over_vchg, $batt_state, $timestamp);
 
     if (($interval > 0) && 
@@ -437,6 +538,12 @@ sub l00http_periobattery_perio {
                     $chg_en =~ s/1/1\/usb/;
                     $chg_en =~ s/2/2\/wall/;
                     $table = "||$battcnt||$level||$vol||$temp||$battmA||$chg_src||$chg_en||$over_vchg||$batt_state||$timestamp||\n" . $table;
+                    # trim if too long
+                    $noln = $table =~ s/\n/\n/g;
+                    while ($noln > $maxreclen) {
+                        $table   =~ s/\n.*?$//;   # trim from the end, oldest
+                        $noln = $table =~ s/\n/\n/g;
+                    }
                 }
             }
         }
@@ -446,6 +553,11 @@ sub l00http_periobattery_perio {
         } else {
             # first time
             $battlog = $tempe;
+        }
+        $noln = $battlog =~ s/\n/\n/g;
+        while ($noln > $maxreclen) {
+            $battlog =~ s/^.*?\n//;   # trim from the start, oldest
+            $noln = $battlog =~ s/\n/\n/g;
         }
         $perltime = time;
         $battpolls++;
