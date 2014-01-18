d14026c7a9c8775763fd68acde35d723dd29475b
l00g33k <l00g33k@gmail.com>
2014-01-18 01:04:53 -0800

    quick path for do.pl

--- l00http_do.pl
+++ l00http_do.pl
@@ -7,6 +7,8 @@
 
 my %config = (proc => "l00http_do_proc",
               desc => "l00http_do_desc");
+my($doplpath);
+$doplpath = '';
 
 sub l00http_do_desc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
@@ -23,6 +25,7 @@ sub l00http_do_proc {
     $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
     my ($rethash, $mypath, $refresh, $refreshtag, $notbare, $fname);
+    my ($quickon, $doplpathnow);
 
     $notbare = 1;
     if (defined ($form->{'bare'})) {
@@ -37,9 +40,6 @@ sub l00http_do_proc {
             $refreshtag = "<meta http-equiv=\"refresh\" content=\"$refresh\"> ";
         }
     }
-#    $arg1 = '';
-#    $arg2 = '';
-#    $arg3 = '';
     if (defined ($form->{'arg1'})) {
         $arg1 = $form->{'arg1'};
     }
@@ -52,9 +52,30 @@ sub l00http_do_proc {
  
     $mypath = $form->{'path'};
 
+    # a lone /do.htm?path=c:/x/del5.pl sets $doplpath
+    if (( defined ($form->{'path'})) &&
+        (!defined ($form->{'arg1'})) &&
+        (!defined ($form->{'arg2'})) &&
+        (!defined ($form->{'arg3'})) &&
+        ($form->{'path'} =~ /\.pl$/)) {
+        $doplpath = $mypath;
+    }
+
+    # handling shortcut
+    if ((!($mypath =~ /\.pl$/)) && ($doplpath ne '')) {
+        # not supplying a .pl
+        # copy path to arg1
+        $form->{'arg1'} = $mypath;
+        $doplpathnow = $doplpath;
+        $quickon = 1;
+    } else {
+        $doplpathnow = $mypath;
+        $quickon = 0;
+    }
+
     # Send HTTP and HTML headers
     if ($notbare) {
-		if (!(($fname) = $mypath =~ /([^\/]+)$/)) {
+		if (!(($fname) = $doplpathnow =~ /([^\/]+)$/)) {
 		    $fname = '(none)';
 		}
         print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>$fname - do</title>\n" . $refreshtag . $ctrl->{'htmlhead2'};
@@ -62,13 +83,13 @@ sub l00http_do_proc {
             print $sock "<a name=\"top\"></a>\n";
             print $sock "$ctrl->{'home'} <a href=\"$ctrl->{'quick'}\">Quick</a> \n";
             print $sock "<a href=\"#end\">Jump to end</a>\n";
-            print $sock "<a href=\"/edit.htm?path=$mypath\">Edit</a>\n";
+            print $sock "<a href=\"/edit.htm?path=$doplpathnow\">Edit</a>\n";
         }
     }
 
-    if ((!($mypath =~ m|/|)) && (!($mypath =~ m|\\|))) {
+    if ((!($doplpathnow =~ m|/|)) && (!($doplpathnow =~ m|\\|))) {
         # try default path
-        $mypath = $ctrl->{'plpath'} . $mypath;
+        $doplpathnow = $ctrl->{'plpath'} . $doplpathnow;
     }
 
     if ($notbare) {
@@ -81,13 +102,11 @@ sub l00http_do_proc {
         }
     }
 
-#`"busybox wget -O /sdcard/l00httpd/del/405S.htm \"http://107.22.209.201/W/RoadDetails.asp?nav=prev&road=104052&p=7.74010&lat=33.699693&lon=-117.800023&z=1\""`;
-#`"busybox wget -O /sdcard/l00httpd/del/405S.htm \"http://107.22.209.201\""`;
 
-    $rethash  = do $mypath;
+    $rethash  = do $doplpathnow;
     if (!defined ($rethash)) {
         if ($!) {
-            print $sock "<hr>Can't read module: $mypath: $!\n";
+            print $sock "<hr>Can't read module: $doplpathnow: $!\n";
         } elsif ($@) {
             print $sock "<hr>Can't parse module: $@\n";
         }
@@ -103,11 +122,17 @@ sub l00http_do_proc {
         print $sock "<form action=\"/do.htm\" method=\"get\">\n";
         print $sock "<input type=\"submit\" name=\"do\" value=\"Do more\">\n";
         print $sock "Path:<input type=\"text\" name=\"path\" size=\"6\" value=\"$mypath\"> \n";
-        print $sock "<input type=\"text\" name=\"refresh\" size=\"2\" value=\"$refresh\"> seci<br>\n";
+        print $sock "<input type=\"text\" name=\"refresh\" size=\"2\" value=\"$refresh\"> sec<br>\n";
         print $sock "Arg1: <input type=\"text\" name=\"arg1\" size=\"12\" value=\"$arg1\"> \n";
         print $sock "Arg2: <input type=\"text\" name=\"arg2\" size=\"12\" value=\"$arg2\"> \n";
         print $sock "Arg3: <input type=\"text\" name=\"arg3\" size=\"12\" value=\"$arg3\"> \n";
         print $sock "</form>\n";
+
+        if ($quickon) {
+            print $sock "Quick path is active. Permanent line:<br>\n";
+            $tmp = "/do.htm?do=Do+more&path=$doplpath&arg1=$form->{'arg1'}";
+            print $sock "<a href=\"$tmp\">$tmp</a><br>\n";
+        }
 
         print $sock "<a name=\"end\"></a>\n";
 
