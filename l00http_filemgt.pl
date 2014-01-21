c7ecb09f17d30f35a55e9abc929cd92cb0214a88
default_git_name <default_git_email>
2014-01-21 06:24:30 +0000

    filemgt: copy tree

--- l00http_filemgt.pl
+++ l00http_filemgt.pl
@@ -8,6 +8,57 @@ use l00backup;
 
 my %config = (proc => "l00http_filemgt_proc",
               desc => "l00http_filemgt_desc");
+my ($treeto, $treefilecnt);
+$treeto = '';
+
+sub copytree {
+    my ($ctrl, $fr, $to) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
+    my ($buf, $file);
+
+    $treefilecnt++;
+    #print "($fr, $to)\n";
+
+    if ((!($to =~ /^\./)) && (!-d $to)) {
+        # assume target doesn't exist, make it
+        # and not hidden .*
+        #print "mkdir >$to<\n";
+        mkdir ($to);
+	}
+
+    if ((!($to =~ /^\./)) && (-d $to)) {
+        # check again, act only target dir exist
+        if (opendir (DIR, $fr)) {
+            foreach $file (readdir (DIR)) {
+                if (($file =~ /^\./) ||
+                    ($file =~ /\.orig$/) ||
+                    ($file =~ /\.tmp$/) ||
+                    ($file =~ /\.bak$/)) {
+                    # skip .*, *.orig, *.bak
+                    next;
+                }
+                if (-d $fr.$file) {
+                    #print "dir >$file<\n";
+                    &copytree($ctrl, "$fr$file/", "$to$file/");
+                } else {
+                    print "cp $to$file\n";
+                    # This is not available on Android: use File::Copy qw(copy); 
+                    # manually copying...
+                    if (open(IN, "<$fr$file")) {
+                        if (open(OU, ">$to$file")) {
+                            local ($/);
+                            $/ = undef;
+                            $buf = <IN>;
+                            print OU $buf;
+                            close(OU);
+                        }
+                        close(IN);
+                    }
+                }
+             }
+         }
+     }
+
+}
 
 sub l00http_filemgt_desc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
@@ -38,6 +89,7 @@ sub l00http_filemgt_proc {
         s/^.+\/([^\/]+)$/$1/;
         print $sock "<a href=\"/ls.htm?path=$form->{'path'}\">$_</a>\n";
     }
+    print $sock "<a href=\"/filemgt.htm?path=$form->{'path'}\">Refresh</a>\n";
     print $sock "<p>\n";
 
     if ((defined ($form->{'delete'})) &&
@@ -51,6 +103,13 @@ sub l00http_filemgt_proc {
         undef $form->{'path'};
     }
 
+    # copy paste target
+    if (defined ($form->{'paste2'})) {
+        if ($ctrl->{'os'} eq 'and') {
+            $buffer = $ctrl->{'droid'}->getClipboard();
+            $form->{'path2'} = $buffer->{'result'};
+        }
+    }
     if ((defined ($form->{'copy'})) &&
         (defined ($form->{'path'}) && 
         (length ($form->{'path'}) > 0)) &&
@@ -98,6 +157,28 @@ sub l00http_filemgt_proc {
         }
     }
 
+    # copy tree
+    if ((defined ($form->{'treeto'}) && 
+        (length ($form->{'treeto'}) > 0))) {
+        $treeto = $form->{'treeto'};
+    }
+    if ((defined ($form->{'copytree'})) &&
+        (defined ($form->{'path'}) && 
+        (length ($form->{'path'}) > 0)) &&
+        (defined ($form->{'treeto'}) && 
+        ((!defined ($form->{'urlonly2'})) || ($form->{'urlonly2'} ne 'on')) &&
+        (length ($form->{'treeto'}) > 0))) {
+        $treefilecnt = 0;
+        &copytree($ctrl, $form->{'path'}, $form->{'treeto'})
+    }
+    # copy tree paste target
+    if (defined ($form->{'pasteto'})) {
+        if ($ctrl->{'os'} eq 'and') {
+            $treeto = $ctrl->{'droid'}->getClipboard();
+            $treeto = $treeto->{'result'};
+        }
+    }
+
     # delete
     if (!defined ($form->{'path'})) {
         $form->{'path'} = '';
@@ -121,20 +202,21 @@ sub l00http_filemgt_proc {
         # if from URL only, use path2
         $path2 = $form->{'path2'};
     } else {
-        if (length ($form->{'path'}) > 0) {
+        if ((length ($form->{'path'}) > 0) &&
+            (length ($form->{'path2'}) == 0)) {
             $path2 = $form->{'path'};
+            # if filename contains extension
             if (!($path2 =~ /\/[^\/.]+$/)) {
+                # insert '.2' before extension as target file
                 $path2 =~ s/(\.[^.]+)$/.2$1/;
             }
-        } else {
-            $path2 = '';
         }
     }
     print $sock "<form action=\"/filemgt.htm\" method=\"post\">\n";
     print $sock "<table border=\"1\" cellpadding=\"3\" cellspacing=\"1\">\n";
     print $sock "<tr><td>\n";
     print $sock "<input type=\"submit\" name=\"copy\" value=\"Copy\">\n";
-    print $sock "<input type=\"submit\" name=\"rename\" value=\"Move\">\n";
+    #print $sock "<input type=\"submit\" name=\"rename\" value=\"Move\">\n";
     print $sock "</td></tr>\n";
     print $sock "<tr><td>\n";
     print $sock "fr: <input type=\"text\" size=\"16\" name=\"path\" value=\"$form->{'path'}\">\n";
@@ -148,6 +230,11 @@ sub l00http_filemgt_proc {
     print $sock "<tr><td>\n";
     print $sock "<input type=\"checkbox\" name=\"urlonly\">Make URL only\n";
     print $sock "</td></tr>\n";
+    if ($ctrl->{'os'} eq 'and') {
+        print $sock "<tr><td>\n";
+        print $sock "<input type=\"submit\" name=\"paste2\" value=\"Paste CB to 'to:'\">\n";
+        print $sock "</td></tr>\n";
+    }
     if (defined ($form->{'copy'}) &&
         (defined ($form->{'urlonly'})) && 
         (defined ($form->{'path'}) && 
@@ -161,7 +248,35 @@ sub l00http_filemgt_proc {
     print $sock "</table><br>\n";
     print $sock "</form>\n";
 
-
+    # copy directory Tree
+	# Remove filename leaving directory as source
+    $form->{'path'} =~ s/\/[^\/]+$/\//;
+    print $sock "<form action=\"/filemgt.htm\" method=\"post\">\n";
+    print $sock "<table border=\"1\" cellpadding=\"3\" cellspacing=\"1\">\n";
+    print $sock "<tr><td>\n";
+    print $sock "<input type=\"submit\" name=\"copytree\" value=\"Copy Tree\">\n";
+    print $sock "</td></tr>\n";
+    print $sock "<tr><td>\n";
+    print $sock "fr: <input type=\"text\" size=\"16\" name=\"path\" value=\"$form->{'path'}\">\n";
+    print $sock "</td></tr>\n";
+    print $sock "<tr><td>\n";
+    print $sock "to: <input type=\"text\" size=\"16\" name=\"treeto\" value=\"$treeto\">\n";
+    print $sock "</td></tr>\n";
+    if ($ctrl->{'os'} eq 'and') {
+        print $sock "<tr><td>\n";
+        print $sock "<input type=\"submit\" name=\"pasteto\" value=\"Paste CB to 'to:'\">\n";
+        print $sock "</td></tr>\n";
+    }
+    print $sock "<tr><td>\n";
+    print $sock "<input type=\"checkbox\" name=\"urlonly2\">Make URL only\n";
+    print $sock "</td></tr>\n";
+    if ((defined ($form->{'urlonly2'})) && ($form->{'urlonly2'} eq 'on')) {
+        print $sock "<tr><td>\n";
+        print $sock "<a href=\"/filemgt.htm?&path=$form->{'path'}&treeto=$form->{'treeto'}&urlonly2=on\">Copy tree URL</a>\n";
+        print $sock "</td></tr>\n";
+    }
+    print $sock "</table><br>\n";
+    print $sock "</form>\n";
 
     # send HTML footer and ends
     print $sock $ctrl->{'htmlfoot'};
