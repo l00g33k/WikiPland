0a56cb8be7a1abefdcb6e7217037aa5d2286caf6
default_git_name <default_git_email>
2014-03-19 00:18:14 +0000

    screen: +-10 steps

--- l00http_screen.pl
+++ l00http_screen.pl
@@ -81,6 +81,8 @@ sub l00http_screen_proc (\%) {
     print $sock "</table>\n";
     print $sock "</form><br>\n";
 
+    print $sock "<a href=\"/screen.htm?inc10=\">+</a> - \n";
+    print $sock "<a href=\"/screen.htm?dec10=\">-</a> - \n";
     for ($ii = 10; $ii < 255; $ii += 10) {
         print $sock "<a href=\"/screen.htm?bright=$ii\">$ii</a> - \n";
     }
