ca97e2223acd45b10224f701578ca9adfd6becb5
default_git_name <default_git_email>
2015-04-08 18:40:04 +0000

    scratch: can now display only bare text

--- l00http_scratch.pl
+++ l00http_scratch.pl
@@ -22,14 +22,22 @@ sub l00http_scratch_proc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     my $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
-    my (@alllines, $line);
+    my (@alllines, $line, $notbare);
+
+    # Do bare display?
+    $notbare = 1;
+    if (defined ($form->{'bare'}) && ($form->{'bare'} eq 'on')) {
+        $notbare = 0;
+    }
 
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>scratch</title>" . $ctrl->{'htmlhead2'};
-    print $sock "$ctrl->{'home'} $ctrl->{'HOME'} \n";
-    print $sock "<a href=\"#end\">Jump to end</a>. \n";
-    print $sock "Go to <a href=\"/clip.htm\">clip</a> - \n";
-    print $sock "<a href=\"/httpd.htm#ram\">RAM files</a><br>\n";
+    if ($notbare) {
+        print $sock "$ctrl->{'home'} $ctrl->{'HOME'} \n";
+        print $sock "<a href=\"#end\">Jump to end</a>. \n";
+        print $sock "Go to <a href=\"/clip.htm\">clip</a> - \n";
+        print $sock "<a href=\"/httpd.htm#ram\">RAM files</a><br>\n";
+    }
 
     if (defined ($form->{'eval'})) {
         $eval = $form->{'eval'};
@@ -68,7 +76,9 @@ sub l00http_scratch_proc {
     }
     $tmp = $scratch;
     if (defined ($form->{'2l00'})) {
-        print $sock "<br>Scratch copied to <a href=\"/view.htm?path=l00://clipboard\">l00://clipboard</a><p>\n";
+        if ($notbare) {
+            print $sock "<br>Scratch copied to <a href=\"/view.htm?path=l00://clipboard\">l00://clipboard</a><p>\n";
+        }
         &l00httpd::l00fwriteOpen($ctrl, 'l00://clipboard');
         &l00httpd::l00fwriteBuf($ctrl, $scratch);
         &l00httpd::l00fwriteClose($ctrl);
@@ -80,33 +90,36 @@ sub l00http_scratch_proc {
     print "scratch: >$scratch<\n", if ($ctrl->{'debug'} >= 5);
 
 #    $tmp =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-    print $sock "<form action=\"/scratch.htm\" method=\"post\">\n";
-    print $sock "<textarea name=\"scratchbuf\" cols=\"$ctrl->{'txtw'}\" rows=\"$ctrl->{'txth'}\">$tmp</textarea>\n";
-    print $sock "<p><input type=\"submit\" name=\"update\" value=\"Set\"> \n";
-    print $sock "<input type=\"submit\" name=\"cbmobi\" value=\"paste CB\"> \n";
-    print $sock "<input type=\"submit\" name=\"cbcopy\" value=\"cp2CB\"> \n";
-    print $sock "<input type=\"submit\" name=\"clear\" value=\"Clr\">\n";
-    print $sock "<br>\n";
-    print $sock "<input type=\"submit\" name=\"append\" value=\"Append\"> \n";
-    print $sock "<input type=\"submit\" name=\"prepend\" value=\"Prepend\">\n";
-    print $sock "<input type=\"submit\" name=\"cburl\" value=\"cp URL 2CB\">\n";
-    print $sock "<input type=\"submit\" name=\"2l00\" value=\"2 ram file\">\n";
+    if ($notbare) {
+        print $sock "<form action=\"/scratch.htm\" method=\"post\">\n";
+        print $sock "<textarea name=\"scratchbuf\" cols=\"$ctrl->{'txtw'}\" rows=\"$ctrl->{'txth'}\">$tmp</textarea>\n";
+        print $sock "<p><input type=\"submit\" name=\"update\" value=\"Set\"> \n";
+        print $sock "<input type=\"submit\" name=\"cbmobi\" value=\"paste CB\"> \n";
+        print $sock "<input type=\"submit\" name=\"cbcopy\" value=\"cp2CB\"> \n";
+        print $sock "<input type=\"submit\" name=\"clear\" value=\"Clr\">\n";
+        print $sock "<br>\n";
+        print $sock "<input type=\"submit\" name=\"append\" value=\"Append\"> \n";
+        print $sock "<input type=\"submit\" name=\"prepend\" value=\"Prepend\">\n";
+        print $sock "<input type=\"submit\" name=\"cburl\" value=\"cp URL 2CB\">\n";
+        print $sock "<input type=\"submit\" name=\"2l00\" value=\"2 ram file\">\n";
 
-    print $sock "<br><input type=\"text\" size=\"10\" name=\"eval\" value=\"$eval\">\n";
-    print $sock "The whole content of the scratch buffer is put in \$_ and then this string is \"eval'ed\", e.g. 'print \$sock \$_' prints the content to this HTML page\n";
-    print $sock "</form>\n";
+        print $sock "<br><input type=\"text\" size=\"10\" name=\"eval\" value=\"$eval\">\n";
+        print $sock "The whole content of the scratch buffer is put in \$_ and then this string is \"eval'ed\", e.g. 'print \$sock \$_' prints the content to this HTML page\n";
+        print $sock "</form>\n";
 
-    print $sock "<form action=\"/scratch.htm\" method=\"get\">\n";
-    print $sock "Refresh as:<br>\n";
-    print $sock "<input type=\"submit\" name=\"mobi\" value=\"Mobilize\">\n";
-    print $sock "<input type=\"submit\" name=\"html\" value=\"HTML\">\n";
-    print $sock "<input type=\"submit\" name=\"text\" value=\"text\">\n";
-    print $sock "<input type=\"submit\" name=\"formatted\" value=\"Formatted\">\n";
-    print $sock "</form><p>\n";
+        print $sock "<form action=\"/scratch.htm\" method=\"get\">\n";
+        print $sock "Refresh as:<br>\n";
+        print $sock "<input type=\"submit\" name=\"mobi\" value=\"Mobilize\">\n";
+        print $sock "<input type=\"submit\" name=\"html\" value=\"HTML\">\n";
+        print $sock "<input type=\"submit\" name=\"text\" value=\"text\">\n";
+        print $sock "<input type=\"submit\" name=\"formatted\" value=\"Formatted\">\n";
+        print $sock "<input type=\"checkbox\" name=\"bare\">Bare";
+        print $sock "</form><p>\n";
 
 
-    print $sock "Send l00://clipboard to <a href=\"/launcher.htm?path=l00://clipboard\">launcher</a>, \n";
-    print $sock "<a href=\"/view.htm?path=l00://clipboard\">View</a> l00://clipboard.<p>\n";
+        print $sock "Send l00://clipboard to <a href=\"/launcher.htm?path=l00://clipboard\">launcher</a>, \n";
+        print $sock "<a href=\"/view.htm?path=l00://clipboard\">View</a> l00://clipboard.<p>\n";
+    }
 
     # get submitted name and print greeting
     if (defined ($form->{'text'})) {
@@ -150,10 +163,12 @@ sub l00http_scratch_proc {
 
     print $sock "$scratchhtml\n";
 
-    print $sock "<p><p><hr>\n";
-    print $sock "<p><p><hr>\n";
-    print $sock "<p><p><hr>\n";
-    print $sock "<hr><a name=\"end\"></a><p><p>\n";
+    if ($notbare) {
+        print $sock "<p><p><hr>\n";
+        print $sock "<p><p><hr>\n";
+        print $sock "<p><p><hr>\n";
+        print $sock "<hr><a name=\"end\"></a><p><p>\n";
+    }
 
  
     # send HTML footer and ends
