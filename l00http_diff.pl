7e55d6c6934540d3a1bd4e2ad57ab87144faaac7
default_git_name <default_git_email>
2015-04-18 18:45:21 +0000

    diff: works good so far

--- l00http_diff.pl
+++ l00http_diff.pl
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 use l00backup;
+use l00httpd;
 
 # Release under GPLv2 or later version by l00g33k@gmail.com, 2010/02/14
 
@@ -8,19 +9,56 @@ use l00backup;
 
 my %config = (proc => "l00http_diff_proc",
               desc => "l00http_diff_desc");
-my ($treeto, $treefilecnt, $treedircnt);
+my ($treeto, $treefilecnt, $treedircnt, $width, $oldfile, $newfile);
 $treeto = '';
+$width = 20;
+#$form->{'path'}
+$oldfile = '';
+$newfile = '';
 
 my (@OLD, @NEW, $OC, $NC, $OLNO, $NLNO, @OA, @NA, %SYMBOL);
 my ($debug);
 
 $debug = 0;
 
+sub l00http_diff_make_outline {
+    my ($oii, $nii) = @_;
+    my ($oout, $nout, $ospc, $tmp, $clip);
+
+
+    if (($oii >= 0) && ($oii <= $#OLD)) {
+        $tmp = sprintf ("%-${width}s", substr($OLD[$oii],0,$width));
+        $ospc = sprintf ("%3d: %-${width}s", $oii + 1, ' ');
+        $ospc =~ s/./ /g;
+        $tmp =~ s/</&lt;/g;
+        $tmp =~ s/>/&gt;/g;
+        $clip = &l00httpd::urlencode ($OLD[$oii]);
+        $clip = "/clip.htm?update=Copy+to+clipboard&clip=$clip";
+        $oout = sprintf ("%3d<a href=\"%s\">:</a> %s", $oii + 1, $clip, $tmp);
+    } else {
+        # make a string of space of same length
+        $oout = '';
+        $ospc = '';
+    }
+    if (($nii >= 0) && ($nii <= $#NEW)) {
+        $tmp = sprintf ("%-${width}s", substr($NEW[$nii],0,$width));
+        $tmp =~ s/</&lt;/g;
+        $tmp =~ s/>/&gt;/g;
+        $clip = &l00httpd::urlencode ($NEW[$nii]);
+        $clip = "/clip.htm?update=Copy+to+clipboard&clip=$clip";
+		$nout = sprintf ("%3d<a href=\"%s\">:</a> %s", $nii + 1, $clip, $tmp);
+    } else {
+        $nout = '';
+    }
+
+    ($oout, $nout, $ospc);
+}
+
 sub l00http_diff_output {
 	my ($ctrl, $oanchor) = @_;
     my $sock = $ctrl->{'sock'};     # dereference network socket
-	my ($ln, $jj, $oii, $nii, $wd, $nfor, $nptr);
-    my ($out, $lastold, $lastnew, $wd);
+	my ($ln, $jj, $oii, $nii, $nfor, $nptr);
+    my ($out, $lastold, $lastnew, $oout, $nout, $ospc);
     my ($blocksize, $blockstart, $mxblocksize, $mxblockstart);
 
 
@@ -85,31 +123,30 @@ sub l00http_diff_output {
     $nii = $OA[$oii];
 
 	$out = '';
-	$wd = 20;
 
     # print forward of largest matched block
 	while (($oii <= $#OLD) || ($nii <= $#NEW)) {
+        # prepare outputs
+        ($oout, $nout, $ospc) = &l00http_diff_make_outline($oii, $nii);
 		# print $sock deleted
 		if (($oii <= $#OLD) && ($OA[$oii] < 0)) {
-			$_ = sprintf (" %3d: %-${wd}s <\n", $oii, $OLD[$oii]);
+			$_ = " $oout &lt;\n";
 			$out .= $_;
 			$oii++;
 			next;
 		}
 		# print $sock added
 		if (($nii <= $#NEW) && ($NA[$nii] < 0)) {
-			$_ = sprintf (" %3d: %-${wd}s  ", $oii, ' ');
-			s/./ /g;
-			s/.$/>/;
-			$_ .= sprintf ("%3d: %-${wd}s\n", $nii, $NEW[$nii]);
+            $_ = " $ospc &gt;";
+			$_ .= "$nout\n";
 			$out .= $_;
 			$nii++;
 			next;
 		}
 		# print $sock identical
 		if (($oii <= $#OLD) && ($nii <= $#NEW) && ($OA[$oii] == $nii)) {
-			$_ = sprintf (" %3d: %-${wd}s =", $oii, $OLD[$oii]);
-			$_ .= sprintf ("%3d: %-${wd}s\n", $nii, $NEW[$nii]);
+			$_ = " $oout =";
+			$_ .= "$nout\n";
 			$out .= $_;
 			$oii++;
 			$nii++;
@@ -117,17 +154,15 @@ sub l00http_diff_output {
 		}
 		# print $sock moved block in NEW
 		if ($NA[$nii] < $oii) {
-			$_ = sprintf (" %3d: %-${wd}s  ", $oii, ' ');
-			s/./ /g;
-			s/.$/[/;
-			$_ .= sprintf ("%3d: %-${wd}s\n", $nii, $NEW[$nii]);
+			$_ = " $ospc [";
+			$_ .= "$nout\n";
 			$out .= $_;
 			$nii++;
 			next;
 		}
 		# print $sock moved block in NEW
 		if ($OA[$oii] > $nii) {
-			$_ = sprintf (" %3d: %-${wd}s ]\n", $oii, $OLD[$oii]);
+			$_ = " $oout ]\n";
 			$out .= $_;
 			$oii++;
 			next;
@@ -144,29 +179,30 @@ sub l00http_diff_output {
 
     $oii = $mxblockstart - 1;
     $nii = $OA[$mxblockstart] - 1;
+
     # print backward from largest matched block
 	while (($oii >= 0) || ($nii >= 0)) {
+        # prepare outputs
+        ($oout, $nout, $ospc) = &l00http_diff_make_outline($oii, $nii);
 		# print $sock deleted
 		if (($oii >= 0) && ($OA[$oii] < 0)) {
-			$_ = sprintf (" %3d: %-${wd}s <\n", $oii, $OLD[$oii]);
+			$_ = " $oout &lt;\n";
 			$out = "$_$out";
 			$oii--;
 			next;
 		}
 		# print $sock added
 		if (($nii >= 0) && ($NA[$nii] < 0)) {
-			$_ = sprintf (" %3d: %-${wd}s  ", $oii, ' ');
-			s/./ /g;
-			s/.$/>/;
-			$_ .= sprintf ("%3d: %-${wd}s\n", $nii, $NEW[$nii]);
+            $_ = " $ospc &gt;";
+			$_ .= "$nout\n";
 			$out = "$_$out";
 			$nii--;
 			next;
 		}
 		# print $sock identical
 		if (($oii >= 0) && ($nii >= 0) && ($OA[$oii] == $nii)) {
-			$_ = sprintf (" %3d: %-${wd}s =", $oii, $OLD[$oii]);
-			$_ .= sprintf ("%3d: %-${wd}s\n", $nii, $NEW[$nii]);
+			$_ = " $oout =";
+			$_ .= "$nout\n";
 			$out = "$_$out";
 			$oii--;
 			$nii--;
@@ -174,17 +210,15 @@ sub l00http_diff_output {
 		}
 		# print $sock moved block in NEW
 		if ($NA[$nii] < $oii) {
-			$_ = sprintf (" %3d: %-${wd}s  ", $oii, ' ');
-			s/./ /g;
-			s/.$/[/;
-			$_ .= sprintf ("%3d: %-${wd}s\n", $nii, $NEW[$nii]);
+			$_ = " $ospc [";
+			$_ .= "$nout\n";
 			$out = "$_$out";
 			$nii--;
 			next;
 		}
 		# print $sock moved block in NEW
 		if ($OA[$oii] > $nii) {
-			$_ = sprintf (" %3d: %-${wd}s ]\n", $oii, $OLD[$oii]);
+			$_ = " $oout ]\n";
 			$out = "$_$out";
 			$oii--;
 			next;
@@ -199,6 +233,7 @@ sub l00http_diff_output {
 		}
     }
 
+
     $out;
 }
 
@@ -208,34 +243,37 @@ sub l00http_diff_output {
 sub l00http_diff_compare {
 	my ($ctrl, $oname, $nname) = @_;
     my $sock = $ctrl->{'sock'};     # dereference network socket
-	my ($ln, $jj, $oii, $nii, $wd, $out, $nfor, $nptr);
+	my ($ln, $jj, $oii, $nii, $out, $nfor, $nptr);
 	my ($text, $mode);
 
     print $sock "<pre>\n";
-    print $sock "old: $oname\n";
-    print $sock "new: $nname\n";
-
+    print $sock "&gt; New file: $nname\n";
+    print $sock "&lt; Old file: $oname\n\n";
 
 
 	# A technique for isolating differences between files
 	# Paul Heckel
 	# http://documents.scribd.com/docs/10ro9oowpo1h81pgh1as.pdf
 
-	$oname = 'c:/x/ram/old.txt';
-	$nname = 'c:/x/ram/new.txt';
-	$oname = '/sdcard/al/w/diff/old.txt';
-	$nname = '/sdcard/al/w/diff/new.txt';
+#$oname = 'c:/x/ram/old.txt';
+#$nname = 'c:/x/ram/new.txt';
+#$oname = '/sdcard/al/w/diff/old.txt';
+#$nname = '/sdcard/al/w/diff/new.txt';
 
 	open (LF, "<$oname") || print $sock "$oname open failed\n";
 	open (RT, "<$nname") || print $sock "$nname open failed\n";
 
+    undef @OLD;
 	while (<LF>) {
-		chomp;
+		s/\r//;
+		s/\n//;
 		push (@OLD, $_);
 	}
 
+    undef @NEW;
 	while (<RT>) {
-		chomp;
+		s/\r//;
+		s/\n//;
 		push (@NEW, $_);
 	}
 
@@ -271,6 +309,7 @@ sub l00http_diff_compare {
 	# $NA[$ln]: if >= 0: OLD file line number of unique and identical $text as $NEW[$ln]
 	#           if == -1: all other cases
 
+    undef %SYMBOL;
 	# Pass 1
 	if ($debug >= 1) {
 		print $sock "Pass 1: Fill SYMBOL table with OLD file content\n";
@@ -411,22 +450,21 @@ sub l00http_diff_compare {
 		print $sock "Pass 6: Output results\n";
 		$oii = 0;
 		$nii = 0;
-		$wd = 20;
 		$out = '';
 		while (($oii <= $#OLD) || ($nii <= $#NEW)) {
 			if ($oii <= $#OLD) {
-				$_ = sprintf ("%3d: OA(%3d) %-${wd}s", $oii, $OA[$oii], $OLD[$oii]);
+				$_ = sprintf ("%3d: OA(%3d) %-${width}s", $oii + 1, substr($OA[$oii],0,$width), substr($OLD[$oii],0,$width));
 				$oii++;
 			} else {
-				$_ = sprintf ("%3d: OA(%3d) %${wd}s", $oii, 0, ' ');
+				$_ = sprintf ("%3d: OA(%3d) %${width}s", $oii + 1, 0, ' ');
 				s/./ /g;
 			}
 			$out .= $_;
 			if ($nii <= $#NEW) {
-				$_ = sprintf ("%3d: NA(%3d) %-${wd}s", $nii, $NA[$nii], $NEW[$nii]);
+				$_ = sprintf ("%3d: NA(%3d) %-${width}s", $nii + 1, substr($NA[$nii],0,$width), substr($NEW[$nii],0,$width));
 				$nii++;
 			} else {
-				$_ = sprintf ("%3d: NA(%3d) %${wd}s", $nii, 0, ' ');
+				$_ = sprintf ("%3d: NA(%3d) %${width}s", $nii + 1, 0, ' ');
 				s/./ /g;
 			}
 			$out .= $_;
@@ -434,7 +472,7 @@ sub l00http_diff_compare {
 		}
 		print $sock $out;
 
-	print $sock "--------------------------\n";
+    	print $sock "--------------------------\n";
 
 		$oii = 0;
 		$nii = 0;
@@ -485,9 +523,6 @@ sub l00http_diff_proc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     my $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
-    my ($buffer, $path2);
-
-$path2 = '';
 
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . $ctrl->{'htmlttl'} . $ctrl->{'htmlhead2'};
@@ -505,98 +540,82 @@ $path2 = '';
         s/^.+\/([^\/]+)$/$1/;
         print $sock "<a href=\"/ls.htm?path=$form->{'path'}\">$_</a>\n";
     }
-    print $sock "<a href=\"/diff.htm?path=$form->{'path'}\">Refresh</a>\n";
+    print $sock "<a href=\"/diff.htm\">Refresh</a>\n";
     print $sock "<p>\n";
 
 
     if (defined ($form->{'debug'})) {
-        $debug = $form->{'debug'};
+        if ($form->{'debug'} =~ /(\d+)/) {
+            $debug = $1;
+        } else {
+            $debug = 5;
+        }
+    }
+
+    if (defined ($form->{'width'})) {
+        if ($form->{'width'} =~ /(\d+)/) {
+            $width = $1;
+        }
     }
 
     # copy paste target
-    if (defined ($form->{'paste2'})) {
-        $form->{'path2'} = &l00httpd::l00getCB($ctrl);
-    }
-    if ((defined ($form->{'copy'})) &&
-        (defined ($form->{'path'}) && 
-        (length ($form->{'path'}) > 0)) &&
-        (defined ($form->{'path2'}) && 
-        (length ($form->{'path2'}) > 0))) {
-        if (defined ($form->{'urlonly'})) {
+    if (defined ($form->{'pasteold'})) {
+        # if pasting old file
+        # this takes precedence over 'path'
+        $oldfile = &l00httpd::l00getCB($ctrl);
+    } elsif (defined ($form->{'pastenew'})) {
+        # if pasting new file
+        # this takes precedence over 'path'
+        $newfile = &l00httpd::l00getCB($ctrl);
+    } elsif (defined ($form->{'path'})) {
+        # could be 'compare' or from ls.htm
+        if (defined ($form->{'pathold'})) {
+            # 'compare' clicked, old file from oldfile field
+            $oldfile = $form->{'pathold'};
         } else {
-            local $/ = undef;
-            if (open (IN, "<$form->{'path'}")) {
-                $buffer = <IN>;
-                close (IN);
-                if ((!defined ($form->{'nobak'})) || ($form->{'nobak'} ne 'on')) {
-                    &l00backup::backupfile ($ctrl, $form->{'path2'}, 1, 5);
-                }
-                open (OU, ">$form->{'path2'}");
-                print OU $buffer;
-                close (OU);
-            }
+            # from ls.htm, push first file to be oldfile
+            $oldfile = $newfile;
         }
-	}
-
-
-
-
-    # copy
-    if (!defined ($form->{'path'})) {
-        $form->{'path'} = '';
+        # new file always from 'path' (field or from ls.htm)
+        $newfile = $form->{'path'};
     }
-    if (defined ($form->{'urlonly'})) {
-        # if from URL only, use path2
-        $path2 = $form->{'path2'};
-    } else {
-        if ((length ($form->{'path'}) > 0) &&
-            (length ($form->{'path2'}) == 0)) {
-            $path2 = $form->{'path'};
-            # if filename contains extension
-            if (!($path2 =~ /\/[^\/.]+$/)) {
-                # insert '.2' before extension as target file
-                $path2 =~ s/(\.[^.]+)$/.2$1/;
-            }
-        }
-    }
+
+
     print $sock "<form action=\"/diff.htm\" method=\"get\">\n";
     print $sock "<table border=\"1\" cellpadding=\"3\" cellspacing=\"1\">\n";
     print $sock "<tr><td>\n";
-    print $sock "<input type=\"submit\" name=\"copy\" value=\"Copy\">\n";
-    #print $sock "<input type=\"submit\" name=\"rename\" value=\"Move\">\n";
+    print $sock "<input type=\"submit\" name=\"compare\" value=\"Compare\">\n";
+    print $sock "</td><td>\n";
+    print $sock "Width: <input type=\"text\" size=\"4\" name=\"width\" value=\"$width\">\n";
     print $sock "</td></tr>\n";
+
     print $sock "<tr><td>\n";
-    print $sock "fr: <input type=\"text\" size=\"16\" name=\"path\" value=\"$form->{'path'}\">\n";
+    print $sock "<input type=\"submit\" name=\"pastenew\" value=\"CB>New:\">";
+    print $sock "</td><td>\n";
+    print $sock "<input type=\"text\" size=\"35\" name=\"path\" value=\"$newfile\">\n";
     print $sock "</td></tr>\n";
+
     print $sock "<tr><td>\n";
-    print $sock "to: <input type=\"text\" size=\"16\" name=\"path2\" value=\"$path2\">\n";
+    print $sock "<input type=\"submit\" name=\"pasteold\" value=\"CB>Old:\">";
+    print $sock "</td><td>\n";
+    print $sock "<input type=\"text\" size=\"35\" name=\"pathold\" value=\"$oldfile\">\n";
     print $sock "</td></tr>\n";
+
     print $sock "<tr><td>\n";
-    print $sock "<input type=\"checkbox\" name=\"nobak\">Do not backup\n";
+#   print $sock "&nbsp;";
+    print $sock "<input type=\"checkbox\" name=\"debug\">debug";
+    print $sock "</td><td>\n";
+    print $sock "<input type=\"checkbox\" name=\"complete\">Display complete files\n";
     print $sock "</td></tr>\n";
-    print $sock "<tr><td>\n";
-    print $sock "<input type=\"checkbox\" name=\"urlonly\">Make URL only\n";
-    print $sock "</td></tr>\n";
-    if ($ctrl->{'os'} eq 'and') {
-        print $sock "<tr><td>\n";
-        print $sock "<input type=\"submit\" name=\"paste2\" value=\"Paste CB to 'to:'\">\n";
-        print $sock "</td></tr>\n";
-    }
-    if (defined ($form->{'copy'}) &&
-        (defined ($form->{'urlonly'})) && 
-        (defined ($form->{'path'}) && 
-        (length ($form->{'path'}) > 0)) &&
-        (defined ($form->{'path2'}) && 
-        (length ($form->{'path2'}) > 0))) {
-        print $sock "<tr><td>\n";
-        print $sock "<a href=\"/diff.htm?copy=Copy&path=$form->{'path'}&path2=$form->{'path2'}&urlonly=on\">Copy URL</a>\n";
-        print $sock "</td></tr>\n";
-    }
     print $sock "</table><br>\n";
     print $sock "</form>\n";
 
 
-	&l00http_diff_compare ($ctrl, '/sdcard/al/w/diff/old.txt', '/sdcard/al/w/diff/new.txt');
+#&l00http_diff_compare ($ctrl, '/sdcard/al/w/diff/old.txt', '/sdcard/al/w/diff/new.txt');
+#&l00http_diff_compare ($ctrl, '/sdcard/al/w/diff/new.txt', '/sdcard/al/w/diff/old.txt');
+    if (defined ($form->{'compare'})) {
+	    &l00http_diff_compare ($ctrl, $oldfile, $newfile);
+    }
 
 
     # send HTML footer and ends
