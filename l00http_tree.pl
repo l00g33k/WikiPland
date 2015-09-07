9413cf42ee3253ed8dbae09582113cd608af6386
l00g33k <l00g33k@gmail.com>
2015-09-07 01:10:43 -0700

    tree: combines size and md5sum file

--- l00http_tree.pl
+++ l00http_tree.pl
@@ -112,7 +112,7 @@ sub l00http_tree_proc {
     my ($buffer, $path2, $path, $file, $cnt, $cntbak, $crc32, $export, $buf);
     my ($dev, $ino, $mode, $nlink, $uid, $gid, $rdev, $time0, $nodir, $nofile, $showbak,
         $size, $atime, $mtimea, $ctime, $blksize, $blocks, $nobytes, $isdir);
-    my (%countext, $ext);
+    my (%countext, $ext, %sizeMd5sum, $md5sum, $fname, $dir);
 
 
     $time0 = time;
@@ -136,25 +136,9 @@ sub l00http_tree_proc {
                 $md5support = 1;
             }
         } elsif ($ctrl->{'os'} eq 'lin') {
-        }
-        
-    }
 
-    # Send HTTP and HTML headers
-    print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . $ctrl->{'htmlttl'} . $ctrl->{'htmlhead2'};
-    print $sock "<a name=\"__top__\"></a>\n";
-    print $sock "$ctrl->{'home'} $ctrl->{'HOME'} - ";
-    if ((defined ($form->{'path'}) && 
-        (length ($form->{'path'}) > 0))) {
-        $_ = $form->{'path'};
-        # keep path only
-        s/\/[^\/]+$/\//;
-        print $sock " Path: <a href=\"/ls.htm?path=$_\">$_</a>\n";
-        $form->{'path'} =~ s/\r//g;
-        $form->{'path'} =~ s/\n//g;
+        }
     }
-    print $sock " <a href=\"#__end__\">jump to end</a><br>\n";
-    print $sock "Links: line#=clip path, path=ls.pl, filename=view.pl<p>\n";
 
     if (!defined ($form->{'path'})) {
         $form->{'path'} = '';
@@ -170,143 +154,202 @@ sub l00http_tree_proc {
         $showbak = '';
     }
 
-    undef @list;
-	$lvl = 0;
-    &l00http_tree_list ($sock, $form->{'path'});
-    print $sock "<pre>";
-	$cnt = 0;
-	$nodir = 0;
-	$nofile = 0;
-	$cntbak = 0;
-	$export = '';
-	$time0 = time;
-    $nobytes = 0;
-    undef %countext;
-    foreach $file (sort @list) {
-        if (defined($form->{'showbak'}) ||
-           (!($file =~ /\.bak$/))) {
-		    if ($file =~ /:$/) {
-		        chop ($file);
-		        $isdir = 1;
-		    } else {
-		        $isdir = 0;
-		    }
-		    $cnt++;
-		    $_ = $file;
-			s/ /%20/g;
-            print $sock "<a href=\"/clip.htm?update=Copy+to+clipboard&clip=$_\">".
-                sprintf("%3d",$cnt)."</a> ";
-		    $_ = $file;
-			($path, $file) = /^(.+\/)([^\/]+)$/;
-            ($dev, $ino, $mode, $nlink, $uid, $gid, $rdev, 
-            $size, $atime, $mtimea, $ctime, $blksize, $blocks)
-                = stat($path.$file);
-            $nobytes += $size;
-            if (defined($form->{'crc32'}) && ($form->{'crc32'} eq 'on')) {
-                if ($isdir) {
-                    $crc32 = 0;
-                    $file = "$file/ &lt;dir&gt;";
-                    $nodir++;
-                } else {
-                    $nofile++;
-                    local $/ = undef;
-                    if(open(IN, "<$path$file")) {
-                        binmode (IN);
-                        $buf = <IN>;
-                        close(IN);
-                    } else {
-                        $buf = '';
-                    }
-                    $crc32 = &l00crc32::crc32($buf);
+
+    # Send HTTP and HTML headers
+    print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . $ctrl->{'htmlttl'} . $ctrl->{'htmlhead2'};
+    print $sock "<a name=\"__top__\"></a>\n";
+    print $sock "$ctrl->{'home'} $ctrl->{'HOME'} - ";
+    print $sock " Path: <a href=\"/ls.htm?path=$form->{'path'}\">$form->{'path'}</a>\n";
+    print $sock " <a href=\"#__end__\">jump to end</a><br>\n";
+    print $sock "Links: line#=clip path, path=ls.pl, filename=view.pl<p>\n";
+
+
+    if (-f $form->{'path'}) {
+        # path is a file
+        if (open(IN, "<$form->{'path'}")) {
+            # get directory
+            $dir = $form->{'path'};
+            # keep path only
+            $dir =~ s/\/[^\/]+$/\//;
+
+            #print $sock "<pre>\n";
+            undef %sizeMd5sum;
+            $cnt = 0;
+            $cntbak = 0;
+            while (<IN>) {
+                s/\n//;
+                s/\r//;
+                # cygwin md5sum puts *./. Delete *
+                s/ \*\.\// .\//;
+                #print $sock "$_\n";
+                if (/^(\d+) \.\/(.+)/) {
+                    $cnt++;
+                    $size = $1;
+                    $fname = $2;
+                    $sizeMd5sum{$fname} = sprintf("|| %8d ||", $size);
+                } elsif (/^([0-9a-fA-F]+) \.\/(.+)/) {
+                    $cntbak++;
+                    $md5sum = $1;
+                    $fname = $2;
+                    $sizeMd5sum{$fname} .= " $md5sum ||";
                 }
-                print $sock sprintf ("<a href=\"/view.htm?path=$path$file\">%8d</a> %08x ", $size, $crc32);
-                $export .= sprintf("|| %8d || %08x || %s ||\n",$size, $crc32, $path.$file);
-            } elsif (($md5support > 0) && defined($form->{'md5'}) && ($form->{'md5'} eq 'on')) {
-                $crc32 = "00000000000000000000000000000000";
-                if ($isdir) {
-                    $file = "$file/ &lt;dir&gt;";
-                    $nodir++;
-                } else {
-                    $nofile++;
-                    if (($ctrl->{'os'} eq 'win') || ($ctrl->{'os'} eq 'cyg')) {
-                        # make command line to call certutil.exe
-                        $_ = "certutil -hashfile \"$path$file\" MD5";
-                        # shell
-                        $_ = `$_`;
-                        # extract the second line
-                        @_ = split ("\n", $_);
-                        $_ = $_[1];
-                        # delete \n, \r, ' '
-                        s/\n//g;
-                        s/\r//g;
-                        s/ //g;
-                        # results
-                        $crc32 = "$_";
-                    } elsif ($ctrl->{'os'} eq 'and') {
-                        $crc32 = &l00Http_tree_proxy($sock, "$path$file");
-                    } elsif ($ctrl->{'os'} eq 'lin') {
-                    }
-                }
-                print $sock sprintf ("<a href=\"/view.htm?path=$path$file\">%8d</a> %s ", $size, $crc32);
-                $export .= sprintf("|| %8d || %s || %s ||\n",$size, $crc32, $path.$file);
-            } else {
-                if ($isdir) {
-                    $file = "$file/ &lt;dir&gt;";
-                    $nodir++;
-                } else {
-                    $nofile++;
-                    # count extension
-                    if ($file =~ /\.([^.]+)$/) {
-                        $ext = $1;
-                    } else {
-                        $ext = '(no ext)';
-                    }
-                    if (defined ($countext{$ext})) {
-                        $countext{$ext}++;
-                    } else {
-                        $countext{$ext} = 1;
-                    }
-                }
-                print $sock sprintf ("<a href=\"/view.htm?path=$path$file\">%8d</a> ", $size);
-                $export .= sprintf("|| %8d || %s ||\n",$size, $path.$file);
             }
-            # show path from base down only
-			$path2 = $path;
-			$path2 =~ s/^$form->{'path'}//;
-            print $sock "<a href=\"/ls.htm?path=$path\">$path2</a>";
-            print $sock "<a href=\"/ls.htm?path=$path$file\">$file</a>\n";
-        } else {
-            $cntbak++;
+            close (IN);
+            print $sock "Read $cnt file size and $cntbak file md5sum<p>\n";
+
+	        $export = '';
+            foreach $fname (sort keys %sizeMd5sum) {
+                $export .= "$sizeMd5sum{$fname} $dir$fname ||\n";
+                #print $sock "$sizeMd5sum{$fname} $dir$fname ||\n";
+            }
+            #print $sock "</pre>\n";
+
+            &l00httpd::l00fwriteOpen($ctrl, 'l00://tree.htm');
+            &l00httpd::l00fwriteBuf($ctrl, "* wiki\n\n$export\n\n");
+            &l00httpd::l00fwriteClose($ctrl);
+            print $sock "<p><a href=\"/view.htm?path=l00://tree.htm\">View raw listing</a><p>\n";
         }
-    }
-    print $sock "</pre>";
+    } else {
+        # path is a directory
+        undef @list;
+	    $lvl = 0;
+        &l00http_tree_list ($sock, $form->{'path'});
+        print $sock "<pre>";
+	    $cnt = 0;
+	    $nodir = 0;
+	    $nofile = 0;
+	    $cntbak = 0;
+	    $export = '';
+	    $time0 = time;
+        $nobytes = 0;
+        undef %countext;
+        foreach $file (sort @list) {
+            if (defined($form->{'showbak'}) ||
+               (!($file =~ /\.bak$/))) {
+		        if ($file =~ /:$/) {
+		            chop ($file);
+		            $isdir = 1;
+		        } else {
+		            $isdir = 0;
+		        }
+		        $cnt++;
+		        $_ = $file;
+			    s/ /%20/g;
+                print $sock "<a href=\"/clip.htm?update=Copy+to+clipboard&clip=$_\">".
+                    sprintf("%3d",$cnt)."</a> ";
+		        $_ = $file;
+			    ($path, $file) = /^(.+\/)([^\/]+)$/;
+                ($dev, $ino, $mode, $nlink, $uid, $gid, $rdev, 
+                $size, $atime, $mtimea, $ctime, $blksize, $blocks)
+                    = stat($path.$file);
+                $nobytes += $size;
+                if (defined($form->{'crc32'}) && ($form->{'crc32'} eq 'on')) {
+                    if ($isdir) {
+                        $crc32 = 0;
+                        $file = "$file/ &lt;dir&gt;";
+                        $nodir++;
+                    } else {
+                        $nofile++;
+                        local $/ = undef;
+                        if(open(IN, "<$path$file")) {
+                            binmode (IN);
+                            $buf = <IN>;
+                            close(IN);
+                        } else {
+                            $buf = '';
+                        }
+                        $crc32 = &l00crc32::crc32($buf);
+                    }
+                    print $sock sprintf ("<a href=\"/view.htm?path=$path$file\">%8d</a> %08x ", $size, $crc32);
+                    $export .= sprintf("|| %8d || %08x || %s ||\n",$size, $crc32, $path.$file);
+                } elsif (($md5support > 0) && defined($form->{'md5'}) && ($form->{'md5'} eq 'on')) {
+                    $crc32 = "00000000000000000000000000000000";
+                    if ($isdir) {
+                        $file = "$file/ &lt;dir&gt;";
+                        $nodir++;
+                    } else {
+                        $nofile++;
+                        if (($ctrl->{'os'} eq 'win') || ($ctrl->{'os'} eq 'cyg')) {
+                            # make command line to call certutil.exe
+                            $_ = "certutil -hashfile \"$path$file\" MD5";
+                            # shell
+                            $_ = `$_`;
+                            # extract the second line
+                            @_ = split ("\n", $_);
+                            $_ = $_[1];
+                            # delete \n, \r, ' '
+                            s/\n//g;
+                            s/\r//g;
+                            s/ //g;
+                            # results
+                            $crc32 = "$_";
+                        } elsif ($ctrl->{'os'} eq 'and') {
+                            $crc32 = &l00Http_tree_proxy($sock, "$path$file");
+                        } elsif ($ctrl->{'os'} eq 'lin') {
+                        }
+                    }
+                    print $sock sprintf ("<a href=\"/view.htm?path=$path$file\">%8d</a> %s ", $size, $crc32);
+                    $export .= sprintf("|| %8d || %s || %s ||\n",$size, $crc32, $path.$file);
+                } else {
+                    if ($isdir) {
+                        $file = "$file/ &lt;dir&gt;";
+                        $nodir++;
+                    } else {
+                        $nofile++;
+                        # count extension
+                        if ($file =~ /\.([^.]+)$/) {
+                            $ext = $1;
+                        } else {
+                            $ext = '(no ext)';
+                        }
+                        if (defined ($countext{$ext})) {
+                            $countext{$ext}++;
+                        } else {
+                            $countext{$ext} = 1;
+                        }
+                    }
+                    print $sock sprintf ("<a href=\"/view.htm?path=$path$file\">%8d</a> ", $size);
+                    $export .= sprintf("|| %8d || %s ||\n",$size, $path.$file);
+                }
+                # show path from base down only
+			    $path2 = $path;
+			    $path2 =~ s/^$form->{'path'}//;
+                print $sock "<a href=\"/ls.htm?path=$path\">$path2</a>";
+                print $sock "<a href=\"/ls.htm?path=$path$file\">$file</a>\n";
+            } else {
+                $cntbak++;
+            }
+        }
+        print $sock "</pre>";
 
-    print $sock "<p>There are $nobytes bytes in $nofile files $nodir directories ";
-    print $sock sprintf("in %d seconds for %d bytes/sec.\n", time - $time0, $nobytes / (time - $time0 + 1));
+        print $sock "<p>There are $nobytes bytes in $nofile files $nodir directories ";
+        print $sock sprintf("in %d seconds for %d bytes/sec.\n", time - $time0, $nobytes / (time - $time0 + 1));
+        print $sock "<p>$cntbak '*.bak' files not shown\n";
 
-    print $sock "<p><table border=\"1\" cellpadding=\"3\" cellspacing=\"1\">\n";
-    print $sock "<tr>\n";
-    print $sock "<td>#</td>\n";
-    print $sock "<td>Extension</td>\n";
-    print $sock "<td>Count</td>\n";
-    print $sock "</tr>\n";
-    $cnt = 1;
-    foreach $ext (sort keys %countext) {
+        print $sock "<p><table border=\"1\" cellpadding=\"3\" cellspacing=\"1\">\n";
         print $sock "<tr>\n";
-        print $sock "<td>$cnt</td>\n";
-        print $sock "<td>$ext</td>\n";
-        print $sock "<td>$countext{$ext}</td>\n";
+        print $sock "<td>#</td>\n";
+        print $sock "<td>Extension</td>\n";
+        print $sock "<td>Count</td>\n";
         print $sock "</tr>\n";
-        $cnt++;
+        $cnt = 1;
+        foreach $ext (sort keys %countext) {
+            print $sock "<tr>\n";
+            print $sock "<td>$cnt</td>\n";
+            print $sock "<td>$ext</td>\n";
+            print $sock "<td>$countext{$ext}</td>\n";
+            print $sock "</tr>\n";
+            $cnt++;
+        }
+        print $sock "</table>\n";
+
+
+        &l00httpd::l00fwriteOpen($ctrl, 'l00://tree.htm');
+        &l00httpd::l00fwriteBuf($ctrl, "* wiki\n\n$export\n\n");
+        &l00httpd::l00fwriteClose($ctrl);
+        print $sock "<p><a href=\"/view.htm?path=l00://tree.htm\">View raw listing</a><p>\n";
     }
-    print $sock "</table>\n";
 
-
-    print $sock "<p>$cntbak '*.bak' files not shown\n";
-    &l00httpd::l00fwriteOpen($ctrl, 'l00://tree.htm');
-    &l00httpd::l00fwriteBuf($ctrl, "* wiki\n\n$export\n\n");
-    &l00httpd::l00fwriteClose($ctrl);
-    print $sock "<p><a href=\"/view.htm?path=l00://tree.htm\">View raw listing</a><p>\n";
 
     print $sock "<form action=\"/tree.htm\" method=\"post\"><hr>\n";
     print $sock "<input type=\"submit\" name=\"submit\" value=\"Path\">\n";
@@ -327,7 +370,16 @@ $form->{'filter'} = 'not implemented';
         }
     }
     print $sock "<br><input type=\"checkbox\" name=\"showbak\" $showbak>Show *.bak too\n";
-    print $sock "</form>\n";
+    print $sock "</form><br>\n";
+
+    print $sock "# md5sum computation can be accelerated by using bash commands as follow:<br>\n";
+    print $sock "du -h<br>\n";
+    print $sock "rm name_stat_md5sum.txt<br>\n";
+    print $sock "time find -name \"*\" -type f -print0 | xargs -0 stat -c \"%s %n\" >> name_stat_md5sum.txt<br>\n";
+    print $sock "time find -name \"*\" -type f -print0 | xargs -0 md5sum >> name_stat_md5sum.txt<br>\n";
+    print $sock "# and send name_stat_md5sum.txt to <a href=\"/tree.htm\">tree.htm</a> for processing<br>\n";
+    print $sock "#speed is approximately 12-26 secs/GB<p>\n";
+
 
     print $sock "<a name=\"__end__\"></a>\n";
     print $sock "<a href=\"#__top__\">jump to top</a>\n";
