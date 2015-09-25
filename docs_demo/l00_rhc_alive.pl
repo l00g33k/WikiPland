0c207801dd7b62bd3e5e146de77e0b5e817b0bc2
default_git_name <default_git_email>
2015-09-25 04:23:02 +0000

    l00_rhc_alive.pl: an example to poll and extract a web page

---
+++ docs_demo/l00_rhc_alive.pl
@@ -0,0 +1,35 @@
+$rhc = 'l00://rhc.htm';
+print $sock "Polling history: <a href=\"/view.htm?path=$rhc\">$rhc</a><p>\n";
+#--------------------------
+$url = 'http://wikipland-l00g33k.rhcloud.com/httpd.htm';
+
+($hdr, $bdy) = &l00wget::wget ($url);
+
+$out = '';
+if (&l00httpd::l00freadOpen($ctrl, $rhc)) {
+    $out = &l00httpd::l00freadAll($ctrl);
+}
+
+$rst = "$ctrl->{'now_string'} hdr " . length($hdr) . " bdy " . length($bdy) . " ";
+foreach $_ (split("\n", $bdy)) {
+    if ((/connected to the WikiPland running/) ||
+       (/Server IP: /)) {
+       s/<.+?>//g;
+       print $sock $_;
+       $rst .= $_;
+    }
+}
+
+$rst =~ s/connected to the WikiPland running //;
+print $sock $rst;
+
+&l00httpd::l00fwriteOpen($ctrl, $rhc);
+&l00httpd::l00fwriteBuf($ctrl, "$rst\n$out");
+&l00httpd::l00fwriteClose($ctrl);
+
+#print $sock "header:<p><pre>\n$hdr\n</pre><p>";
+#$bdy =~ s/</&lt/g;
+#$bdy =~ s/>/&gt/g;
+#print $sock "body:<p><pre>\n$bdy\n</pre><p>";
+
+
