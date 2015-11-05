5cc56974169f2fa2c1752d7a41d16b26342086c5
l00g33k <l00g33k@gmail.com>
2015-11-04 22:36:07 -0800

    timelog: add reminder display

--- l00http_timelog.pl
+++ l00http_timelog.pl
@@ -8,8 +8,8 @@ use l00backup;
 
 my %config = (proc => "l00http_timelog_proc",
               desc => "l00http_timelog_desc");
-my ($buffer, $hislen, $justsaved, $lastbuf);
-$hislen = 2;
+my ($buffer, $bufhislen, $justsaved, $lastbuf);
+$bufhislen = 2;
 $justsaved = 0;
 $lastbuf = '';
 
@@ -26,8 +26,8 @@ sub l00http_timelog_proc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     my $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
-    my (@alllines, $line, $lineno, $ii, $battlvl, $filecontent);
-    my (@quicktext);
+    my (@bufhistory, $line, $lineno, $ii, $filecontent);
+    my (@quicktext, $reminder);
 
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . $ctrl->{'htmlttl'} .$ctrl->{'htmlhead2'};
@@ -37,80 +37,71 @@ sub l00http_timelog_proc {
         print $sock "Path: <a href=\"/ls.htm?path=$form->{'path'}\">$form->{'path'}</a> \n";
     }
     print $sock "<a name=\"__top__\"></a> ";
-    if ($ctrl->{'os'} eq 'and') {
-        $battlvl = $ctrl->{'droid'}->batteryGetLevel(); 
-    }
     print $sock "<a href=\"#__end__\">Jump to end</a><br>\n";
 
     $buffer = "";
     if (defined ($form->{'save'}) || defined ($form->{'quick'})) {
+        # button clicked: 'Save to file', quick cuts,
         if (($justsaved == 0) &&
             (defined ($form->{'buffer'})) &&
             ((defined ($form->{'path'})) && 
-            (length ($form->{'path'}) > 0))) {
-            if ($form->{'buffer'} ne $lastbuf) {
-                if (defined ($form->{'quick'})) {
-                    $form->{'buffer'} .= $form->{'quick'};
-                }
-                $lastbuf = $form->{'buffer'};
-                &l00backup::backupfile ($ctrl, $form->{'path'}, 1, 9);
-                if (open (IN, "<$form->{'path'}")) {
-                    local $/;
-                    #09/15/10 12:41:56 
-                    my $match = substr ($form->{'buffer'}, 0, 17);
-                    #print "match )$match(\n";
-                    $/ = undef;
-                    $buffer = <IN>;
-                    close (IN);
-                    open (OUT, ">$form->{'path'}");
-                    foreach $line (split ("\n", $buffer)) {
-                        if ($match eq substr ($line, 0, 17)) {
-                            last;
-                        }
-                        $line =~ s/\r//g;
-                        $line =~ s/\n//g;
-                        print OUT "$line\n";
+            (length ($form->{'path'}) > 0)) && 
+            ($form->{'buffer'} ne $lastbuf)) {
+            # not repeat, got buffer and path
+            if (defined ($form->{'quick'})) {
+                $form->{'buffer'} .= $form->{'quick'};
+            }
+            $lastbuf = $form->{'buffer'};
+            &l00backup::backupfile ($ctrl, $form->{'path'}, 1, 9);
+            if (open (IN, "<$form->{'path'}")) {
+                local $/;
+                #09/15/10 12:41:56 
+                my $match = substr ($form->{'buffer'}, 0, 17);
+                #print "match )$match(\n";
+                $/ = undef;
+                $buffer = <IN>;
+                close (IN);
+                open (OUT, ">$form->{'path'}");
+                foreach $line (split ("\n", $buffer)) {
+                    if ($match eq substr ($line, 0, 17)) {
+                        last;
                     }
-                    foreach $line (split ("\n", $form->{'buffer'})) {
-                        $line =~ s/\r//g;
-                        $line =~ s/\n//g;
-                        print OUT "$line\n";
-                    }
-                    close (OUT);
-                } else {
-                    print $sock "Unable to write '$form->{'path'}'<p>\n";
+                    $line =~ s/\r//g;
+                    $line =~ s/\n//g;
+                    print OUT "$line\n";
                 }
+                foreach $line (split ("\n", $form->{'buffer'})) {
+                    $line =~ s/\r//g;
+                    $line =~ s/\n//g;
+                    print OUT "$line\n";
+                }
+                close (OUT);
+            } else {
+                print $sock "Unable to write '$form->{'path'}'<p>\n";
             }
         }
     } else {
         # disallow 2 succesive save in a row; the 2nd save likely is refresh
         # get newtime to reset
     }
-#08/09/10  8:54:23 ad tc
-#08/09/10  9:35:23 ad em
-#08/09/10 10:04:45 mgt joe task
+
+    # scan time entries for the last $bufhislen (2) lines.
+    #08/09/10  8:54:23 ad tc
+    #08/09/10  9:35:23 ad em
+    #08/09/10 10:04:45 mgt joe task
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime (time);
-    for ($line = 0; $line < $hislen; $line++) {
-        $alllines [$line] = '';
-    }
-    $buffer = '';
+    # clear buffer history
+    undef @bufhistory;
+    $buffer = '';   # edit buffer content
     if (open (IN, "<$form->{'path'}")) {
-        $line = 0;
         while (<IN>) {
-            $alllines [$line] = $_;
-            $line++;
-            if ($line >= $hislen) {
-                $line = 0;
+            push(@bufhistory, $_);
+            if ($#bufhistory >= $bufhislen) {
+                shift(@bufhistory);
             }
         }
         close (IN);
-        for ($ii = 0; $ii < $hislen; $ii++) {
-            $buffer .= $alllines [$line];
-            $line++;
-            if ($line >= $hislen) {
-                $line = 0;
-            }
-        }
+        $buffer .= join('', @bufhistory);
     }
     $buffer .= sprintf ("%02d/%02d/%02d %2d:%02d:%02d ", 
         $mon+1, $mday, $year-100, $hour, $min, $sec);
@@ -119,18 +110,32 @@ sub l00http_timelog_proc {
     $filecontent = '';
     $lineno = 1;
     undef @quicktext;
+    $reminder = '';
     if (open (IN, "<$form->{'path'}")) {
         $filecontent = "<pre>\n";
         while (<IN>) {
             s/\r//g;
             s/\n//g;
-            if (/^#(.+)/) {
+            if (/^\d{8,8} \d{6,6} (.+)/) {
+                if ($reminder eq '') {
+                    $reminder = "$1";
+                } else {
+                    $reminder .= "\n$1";
+                }
+            }
+            if (/^#\d{8,8} \d{6,6} (.+)/) {
+                # skip deleted item
+            } elsif (/^#(.+)/) {
+                # else # is quick text
                 push (@quicktext, $1);
             }
             $filecontent .= sprintf ("%04d: ", $lineno++) . "$_\n";
         }
         close (IN);
         $filecontent .= "</pre>\n";
+    }
+    if ($reminder ne '') {
+        print $sock "<pre>$reminder</pre>\n";
     }
 
     print $sock "<form action=\"/timelog.htm\" method=\"post\">\n";
