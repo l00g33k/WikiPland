db8c5ef4b16486c7e6367d2a6751fe8e51f66403
l00g33k <l00g33k@gmail.com>
2016-10-02 16:58:51 -0700

    readgraph: work in progress

--- l00http_readgraph.pl
+++ l00http_readgraph.pl
@@ -10,7 +10,13 @@ use l00svg;
 
 my %config = (proc => "l00http_readgraph_proc",
               desc => "l00http_readgraph_desc");
-my ($lastx, $lasty, $lastoff);
+my ($lastx, $lasty, $lastoff, $lastpath, $xoff, $yoff);
+
+$lastx = undef;
+$lasty = undef;
+$lastpath = undef;
+$xoff = 4;
+$yoff = 2;
 
 sub l00http_readgraph_desc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
@@ -23,34 +29,39 @@ sub l00http_readgraph_proc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     my $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
-    my ($xpix, $ypix);
+    my ($xpix, $ypix, $pname, $fname, $dx, $dy);
 
 
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>l00httpd</title>" . $ctrl->{'htmlhead2'};
 
-    print $sock "<form action=\"/readgraph.htm\" method=\"get\">\n";
-#    print $sock "<input type=image style=\"float:none\" src=\"/svg.htm?graph=demo\"><br>\n";
-    print $sock "<input type=image style=\"float:none\" src=\"/ls.htm/Screenshot_2016-02-02-07-20-41.png?path=/sdcard/Pictures/Screenshots/Screenshot_2016-02-02-07-20-41.png\"><br>\n";
-# /sdcard/Pictures/Screenshots/Screenshot_2016-02-02-07-20-41.png
-#http://127.0.0.1:30337
-#/ls.htm/Screenshot_2016-02-02-07-20-41.png?path=/sdcard/Pictures/Screenshots/Screenshot_2016-02-02-07-20-41.png
-
-    if (defined ($form->{'x'})) {
-        $xpix = $form->{'x'} + 4;
-        $ypix = $form->{'y'} + 2;
-        print $sock "<div style=\"position: absolute; left:$xpix"."px; top:$ypix"."px;\">\n";
-        print $sock "<font color=\"red\">+</font></div>\n";
+    if (defined ($form->{'path'}) &&
+        (($pname, $fname) = $form->{'path'} =~ /^(.+\/)([^\/]+)$/)) {
+        print $sock "<form action=\"/readgraph.htm\" method=\"get\">\n";
+        print $sock "<input type=\"hidden\" name=\"path\" value=\"$pname$fname\">\n";
+        print $sock "<input type=image style=\"float:none\" src=\"/ls.htm/$fname?path=$pname$fname\"><br>\n";
+        if (defined ($form->{'x'})) {
+            $xpix = $form->{'x'} + $xoff;
+            $ypix = $form->{'y'} + $yoff;
+            print $sock "<div style=\"position: absolute; left:$xpix"."px; top:$ypix"."px;\">\n";
+            print $sock "<font color=\"red\">+</font></div>\n";
+        }
+        print $sock "</form>\n";
     }
 
-    print $sock "<input type=\"hidden\" name=\"graph\" value=\"demo\">\n";
-    print $sock "<input type=\"hidden\" name=\"view\">\n";
-    print $sock "</form>\n";
 
     print $sock "$ctrl->{'home'} $ctrl->{'HOME'}\n";
     print $sock "Click graph above.\n";
-    if (defined ($form->{'x'})) {
+    if (defined ($form->{'path'}) && (defined ($form->{'x'}))) {
         print $sock "You clicked: ($form->{'x'},$form->{'y'})<br>\n";
+        if ($lastpath ne undef) {
+            $dx = $xpix - $lastx;
+            $dy = $ypix - $lasty;
+            print $sock "Delta: ($dx, $dy)<br>\n";
+        }
+        $lastx = $xpix;
+        $lasty = $ypix;
+        $lastpath = $form->{'path'};
     }
 
     # send HTML footer and ends
