980642119617cb61507299290d4f33738871d4ce
l00g33k <l00g33k@gmail.com>
2016-10-09 13:57:32 -0700

    l00httpd.pl: move exmaple RAM file to the top in list for each access

--- l00httpd.pl
+++ l00httpd.pl
@@ -139,7 +139,7 @@ $ctrl{'lssize'}  = "launcher";
 $ctrl{'blogwd'}  = 64;
 $ctrl{'blogmaxln'}  = 50;
 $ctrl{'noclinav'}  = 1;
-$ctrl{'l00file'}->{'l00://ram'} = "A sample ram file.\nContent is lost when shutdown\nChange 'ram' for a separate ram file";
+$ctrl{'l00file'}->{'l00://_notes'} = "A sample ram file.\nContent is lost when shutdown\nChange 'ram' for a separate ram file";
 
 $nopwtimeout = 0;
 
@@ -1666,7 +1666,7 @@ while(1) {
                     $tmp = $ctrl{'l00file'};
 
                     foreach $_ (sort keys %$tmp) {
-                        if (($_ eq 'l00://ram') ||
+                        if (($_ eq 'l00://_notes') ||
                            (defined($ctrl{'l00file'}->{$_}) &&
                             (length($ctrl{'l00file'}->{$_}) > 0))) {
                             my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst);
