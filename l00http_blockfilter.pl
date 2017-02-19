8963c702fb10976cd1e31164cf3a0f1e9c9bb7d6
l00g33k <l00g33k@gmail.com>
2017-02-19 00:29:33 -0800

    blockfilter: renamed from md5sizediffproc; as paste buttons

--- l00http_blockfilter.pl
+++ l00http_blockfilter.pl
@@ -5,25 +5,26 @@ use warnings;
 
 # this is a simple template, a good starting point to make your own modules
 
-my %config = (proc => "l00http_md5sizediffproc_proc",
-              desc => "l00http_md5sizediffproc_desc");
-my ($required, $exclude);
+my %config = (proc => "l00http_blockfilter_proc",
+              desc => "l00http_blockfilter_desc");
+my ($required, $exclude, $blockend);
 $required = '';
 $exclude = '';
+$blockend = '';
 
-sub l00http_md5sizediffproc_desc {
+sub l00http_blockfilter_desc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     # Descriptions to be displayed in the list of modules table
     # at http://localhost:20337/
-    "md5sizediffproc: Special processor for md5sizediff text outputs";
+    "blockfilter: A block display filter";
 }
 
-sub l00http_md5sizediffproc_proc {
+sub l00http_blockfilter_proc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     my $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
     my ($cnt, $requiredhits, $excludehits, $output, $thisblock, $condition);
-    my ($blkdisplayed, $nonumblock);
+    my ($blkdisplayed, $nonumblock, $blockendhits, $hitlines, $hitlinesthis);
 
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>Android startActivity</title>" .$ctrl->{'htmlhead2'};
@@ -31,7 +32,20 @@ sub l00http_md5sizediffproc_proc {
     print $sock "$ctrl->{'home'} $ctrl->{'HOME'} <a href=\"#__end__\">jump to end</a> - \n";
     print $sock "Path: <a href=\"/ls.htm?path=$form->{'path'}\">$form->{'path'}</a><br>\n";
 
+    if (defined ($form->{'blockpaste'})) {
+        $blockend = &l00httpd::l00getCB($ctrl);
+    }
+    if (defined ($form->{'requiredpaste'})) {
+        $required = &l00httpd::l00getCB($ctrl);
+    }
+    if (defined ($form->{'excludepaste'})) {
+        $exclude = &l00httpd::l00getCB($ctrl);
+    }
+
     if (defined ($form->{'process'})) {
+        if (defined ($form->{'blockend'})) {
+            $blockend = $form->{'blockend'};
+        }
         if (defined ($form->{'required'})) {
             $required = $form->{'required'};
         }
@@ -40,7 +54,7 @@ sub l00http_md5sizediffproc_proc {
         }
     }
 
-    print $sock "<form action=\"/md5sizediffproc.htm\" method=\"get\">\n";
+    print $sock "<form action=\"/blockfilter.htm\" method=\"get\">\n";
     print $sock "<table border=\"3\" cellpadding=\"3\" cellspacing=\"1\">\n";
 
     print $sock "<tr><td>\n";
@@ -50,15 +64,24 @@ sub l00http_md5sizediffproc_proc {
     print $sock "</td></tr>\n";
 
     print $sock "<tr><td>\n";
-    print $sock "Required condition (1 per line)\n";
+    print $sock "<input type=\"submit\" name=\"blockpaste\" value=\"Block\">\n";
+    print $sock "ending (1 per line)\n";
     print $sock "</td><td>\n";
-    print $sock "<textarea name=\"required\" cols=$ctrl->{'txtw'} rows=$ctrl->{'txth'}>$required</textarea>\n";
+    print $sock "<textarea name=\"blockend\" cols=24 rows=7>$blockend</textarea>\n";
     print $sock "</td></tr>\n";
 
     print $sock "<tr><td>\n";
-    print $sock "Exclude condition (1 per line)\n";
+    print $sock "<input type=\"submit\" name=\"requiredpaste\" value=\"Required\">\n";
+    print $sock "condition (1 per line)\n";
     print $sock "</td><td>\n";
-    print $sock "<textarea name=\"exclude\" cols=$ctrl->{'txtw'} rows=$ctrl->{'txth'}>$exclude</textarea>\n";
+    print $sock "<textarea name=\"required\" cols=24 rows=7>$required</textarea>\n";
+    print $sock "</td></tr>\n";
+
+    print $sock "<tr><td>\n";
+    print $sock "<input type=\"submit\" name=\"excludepaste\" value=\"Exclude\">\n";
+    print $sock "condition (1 per line)\n";
+    print $sock "</td><td>\n";
+    print $sock "<textarea name=\"exclude\" cols=24 rows=7>$exclude</textarea>\n";
     print $sock "</td></tr>\n";
 
     print $sock "</table><br>\n";
@@ -68,7 +91,7 @@ sub l00http_md5sizediffproc_proc {
     if (defined ($form->{'process'}) &&
         defined ($form->{'path'}) &&
         (&l00httpd::l00freadOpen($ctrl, $form->{'path'}))) {
-        &l00httpd::l00fwriteOpen($ctrl, 'l00://md5sizediffproc.txt');
+        &l00httpd::l00fwriteOpen($ctrl, 'l00://blockfilter.txt');
 
         $cnt = 0;
         $blkdisplayed = 0;
@@ -78,16 +101,27 @@ sub l00http_md5sizediffproc_proc {
         $excludehits = 0;
         $thisblock = '';
         $nonumblock = '';
+        $hitlines = 0;
         while ($_ = &l00httpd::l00freadLine($ctrl)) {
             $cnt++;
             $thisblock .= sprintf ("%05d: %s", $cnt, $_);
+            $hitlinesthis++;
             $nonumblock .= $_;
-            if (/^ *$/) {
+
+            $blockendhits = 0;
+            foreach $condition (split("\n", $blockend)) {
+                if (/$condition/) {
+                    $blockendhits++;
+                }
+            }
+
+            if ($blockendhits > 0) {
                 # blank line is end of block
                 # do we print?
                 if (($requiredhits > 0) &&
                     ($excludehits == 0)) {
                     $blkdisplayed++;
+                    $hitlines += $hitlinesthis;
                     $output .= $thisblock;
                     &l00httpd::l00fwriteBuf($ctrl, "$nonumblock");
                 }
@@ -95,6 +129,7 @@ sub l00http_md5sizediffproc_proc {
                 $excludehits = 0;
                 $thisblock = '';
                 $nonumblock = '';
+                $hitlinesthis = 0;
             } else {
                 foreach $condition (split("\n", $required)) {
                     if (/$condition/) {
@@ -109,8 +144,8 @@ sub l00http_md5sizediffproc_proc {
             }
         }
         &l00httpd::l00fwriteClose($ctrl);
-        print $sock "Displaying $blkdisplayed blocks. View ".
-            "<a href=\"/view.htm?path=l00://md5sizediffproc.txt\">l00://md5sizediffproc.txt</a><br>\n";
+        print $sock "Displaying $blkdisplayed blocks and $hitlines lines. View ".
+            "<a href=\"/view.htm?path=l00://blockfilter.txt\">l00://blockfilter.txt</a><br>\n";
         print $sock "<pre>$output</pre>\n";
         print $sock "<a name=\"__end__\"></a>\n";
         print $sock "<a href=\"#__top__\">jump to top</a>\n";
