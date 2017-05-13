c6e36a26725712507ac939279724cf63395d98c6
l00g33k <l00g33k@gmail.com>
2017-05-12 22:30:46 -0700

    kml2gmap: change default zoom level

--- l00http_kml2gmap.pl
+++ l00http_kml2gmap.pl
@@ -490,7 +490,7 @@ sub l00http_kml2gmap_proc {
             $zoom = 1;
             if ($span > 1e-9) {
                 while (1) {
-                    if ($span * 2 ** $zoom > 400) {
+                    if ($span * 2 ** $zoom > 200) {
                         last;
                     }
                     $zoom++;
