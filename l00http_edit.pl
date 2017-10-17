2a931e09ddfddb17c4e1729387c1a02da9b9037a
l00g33k <l00g33k@gmail.com>
2017-10-16 23:22:05 -0700

    minor

--- l00http_edit.pl
+++ l00http_edit.pl
@@ -74,7 +74,7 @@ sub l00http_edit_proc2 {
 
     if (defined ($form->{'path'})) {
         ($pname, $fname) = $form->{'path'} =~ /^(.+[\\\/])([^\\\/]+)$/;
-        print $sock "<a href=\"/clip.htm?update=Copy+to+clipboard&clip=:hide+edit+$form->{'path'}%0D\">Path</a>: ";
+        print $sock "<a href=\"/clip.htm?update=Copy+to+clipboard&clip=$form->{'path'}%0D\">Path</a>: ";
         print $sock " <a href=\"/ls.htm?path=$pname\">$pname</a>";
         print $sock "<a href=\"/ls.htm?path=$form->{'path'}\">$fname</a> \n";
         print $sock " <a href=\"/ls.htm?path=$form->{'path'}&editline=on\">Edit line link</a>\n";
