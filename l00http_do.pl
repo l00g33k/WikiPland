ead431634c929be4a35cba3d1158d87e913c062d
l00g33k <l00g33k@gmail.com>
2017-10-25 10:30:50 -0700

    do: add hide form and raw mode

--- l00http_do.pl
+++ l00http_do.pl
@@ -15,28 +15,25 @@ sub l00http_do_desc {
     "do: Perl 'do' the destination file";
 }
 
-my ($arg1, $arg2, $arg3, $doplpath, $doplpathset, $hide);
+my ($arg1, $arg2, $arg3, $doplpath, $doplpathset, $hide, $bare);
 $arg1 = '';
 $arg2 = '';
 $arg3 = '';
 $doplpath = '';
 $doplpathset = 0;
-$hide = 'unchecked';
+$hide = '';
+$bare = '';
 
 sub l00http_do_proc {
     my ($main);
     ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
     $sock = $ctrl->{'sock'};     # dereference network socket
     my $form = $ctrl->{'FORM'};     # dereference FORM data
-    my ($rethash, $mypath, $refresh, $refreshtag, $notbare, $fname);
+    my ($rethash, $mypath, $refresh, $refreshtag, $fname);
     my ($doplpathnow);
 
     print "MOD: $config{'desc'}: Entered do\n", if ($debug >= 5);
 
-    $notbare = 1;
-    if (defined ($form->{'bare'})) {
-        $notbare = 0;
-    }
 
     $refresh = '';
     $refreshtag = '';
@@ -55,12 +52,13 @@ sub l00http_do_proc {
     if (defined ($form->{'arg3'})) {
         $arg3 = $form->{'arg3'};
     }
-    $hide = 'unchecked';
-    if (defined ($form->{'hide'})) {
-        if ($form->{'hide'} eq 'on') {
-            $hide = 'checked';
-            $notbare = 0;
-        }
+    $hide = '';
+    if ((defined ($form->{'hide'})) && ($form->{'hide'} eq 'on')) {
+        $hide = 'checked';
+    }
+    $bare = '';
+    if ((defined ($form->{'bare'})) && ($form->{'bare'} eq 'on')) {
+        $bare = 'checked';
     }
 
     # push args
@@ -72,7 +70,6 @@ sub l00http_do_proc {
  
     $mypath = $form->{'path'};
 
-    # a lone /do.htm?path=c:/x/del5.pl sets $doplpath
     if ((defined ($form->{'set'})) &&
         (defined ($form->{'path'}))) {
         $doplpath = $form->{'path'};
@@ -92,17 +89,21 @@ sub l00http_do_proc {
     }
 
     # Send HTTP and HTML headers
-    if ($notbare) {
+    if ($bare ne 'checked') {
 		if (!(($fname) = $doplpathnow =~ /([^\/]+)$/)) {
 		    $fname = '(none)';
 		}
-        print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>$fname - do</title>\n" . $refreshtag . $ctrl->{'htmlhead2'};
-        if ($refresh eq '') {
-            print $sock "<a name=\"top\"></a>\n";
-            print $sock "$ctrl->{'home'} $ctrl->{'HOME'} \n";
-            print $sock "<a href=\"#end\">Jump to end</a>\n";
-            print $sock "<a href=\"/edit.htm?path=$doplpathnow\">Ed</a>\n";
-            print $sock "<a href=\"/view.htm?path=$doplpathnow\">Vw</a>\n";
+        if ($hide ne 'checked') {
+            print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>$fname - do</title>\n" . $refreshtag . $ctrl->{'htmlhead2'};
+            if ($refresh eq '') {
+                print $sock "<a name=\"top\"></a>\n";
+                print $sock "$ctrl->{'home'} $ctrl->{'HOME'} \n";
+                print $sock "<a href=\"#end\">Jump to end</a>\n";
+                print $sock "<a href=\"/edit.htm?path=$doplpathnow\">Ed</a>\n";
+                print $sock "<a href=\"/view.htm?path=$doplpathnow\">Vw</a>\n";
+            }
+        } else {
+            print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>$fname - do</title>\n" . $refreshtag . $ctrl->{'htmlhead2'};
         }
     }
 
@@ -111,8 +112,8 @@ sub l00http_do_proc {
         $doplpathnow = $ctrl->{'plpath'} . $doplpathnow;
     }
 
-    if ($notbare) {
-        if ($refresh eq '') {
+    if ($bare ne 'checked') {
+        if (($refresh eq '') && ($hide ne 'checked')) {
             print $sock "<form action=\"/do.htm\" method=\"get\">\n";
             print $sock "<input type=\"submit\" name=\"do\" value=\"Do\">\n";
             if ($doplpathset) {
@@ -141,37 +142,62 @@ sub l00http_do_proc {
         }
     } else {
         # default to disabled to non local clients^M
-        if ($notbare) {
-            print $sock "<hr>Run completed\n";
+        if ($bare ne 'checked') {
+            print $sock "<hr>Run completed<br>\n";
         }
     }
-    if ($notbare) {
-        print $sock "<p>'sec': insert HTML tag to automatically reload the page at specified seconds interval.<br><a href=\"#top\">Jump to top</a><p>\n";
+    if ($bare ne 'checked') {
+        if ($hide ne 'checked') {
+            print $sock "<p>'sec': insert HTML tag to automatically reload the page at specified seconds interval.<br><a href=\"#top\">Jump to top</a><p>\n";
 
-        print $sock "<form action=\"/do.htm\" method=\"get\">\n";
-        print $sock "<input type=\"submit\" name=\"do\" value=\"Do more\">\n";
-        print $sock "Path:<input type=\"text\" name=\"path\" size=\"6\" value=\"$mypath\"> \n";
-        print $sock "<input type=\"text\" name=\"refresh\" size=\"2\" value=\"$refresh\"> sec.\n";
-        print $sock "<input type=\"checkbox\" name=\"hide\" $hide>Hide framework<br>\n";
-        print $sock "Arg1: <input type=\"text\" name=\"arg1\" size=\"12\" value=\"$arg1\"> \n";
-        print $sock "Arg2: <input type=\"text\" name=\"arg2\" size=\"12\" value=\"$arg2\"> \n";
-        print $sock "Arg3: <input type=\"text\" name=\"arg3\" size=\"12\" value=\"$arg3\"> \n";
-        print $sock "</form>\n";
-
-        if ($doplpathset) {
-            print $sock "Quick URL is active. Full URL is:<br>\n";
-            $tmp = "/do.htm?do=Do+more&path=$doplpath&arg1=$form->{'arg1'}";
-            print $sock "<a href=\"$tmp\">$tmp</a><br>\n";
+            print $sock "<form action=\"/do.htm\" method=\"get\">\n";
+            print $sock "<input type=\"submit\" name=\"do\" value=\"Do more\">\n";
+            print $sock "Path:<input type=\"text\" name=\"path\" size=\"6\" value=\"$mypath\"> \n";
+            print $sock "<input type=\"text\" name=\"refresh\" size=\"2\" value=\"$refresh\"> sec.\n";
+            print $sock "<input type=\"checkbox\" name=\"hide\" $hide>Hide form.\n";
+            print $sock "<input type=\"checkbox\" name=\"bare\" $bare>bare<br>\n";
+            print $sock "Arg1: <input type=\"text\" name=\"arg1\" size=\"12\" value=\"$arg1\"> \n";
+            print $sock "Arg2: <input type=\"text\" name=\"arg2\" size=\"12\" value=\"$arg2\"> \n";
+            print $sock "Arg3: <input type=\"text\" name=\"arg3\" size=\"12\" value=\"$arg3\"> \n";
+            print $sock "</form>\n";
         }
 
-        print $sock "<form action=\"/do.htm\" method=\"get\">\n";
-        print $sock "<input type=\"submit\" name=\"pushcb\" value=\"CB\">\n";
-        print $sock "-&gt; Arg1 -&gt; Arg2 -&gt; Arg3<p>\n";
-        print $sock "<input type=\"submit\" name=\"clear\" value=\"Clear\">\n";
-        print $sock "<input type=\"submit\" name=\"set\" value=\"Set\">\n";
-        print $sock "Quick URL to: $form->{'path'}\n";
-        print $sock "<input type=\"hidden\" name=\"path\" value=\"$form->{'path'}\">\n";
-        print $sock "</form>\n";
+        if ($hide ne 'checked') {
+            if ($doplpathset) {
+                print $sock "Quick URL is active. Full URL is:<br>\n";
+                $tmp = "/do.htm?do=Do+more&path=$doplpath&arg1=$form->{'arg1'}";
+                print $sock "<a href=\"$tmp\">$tmp</a><br>\n";
+            }
+
+            print $sock "<form action=\"/do.htm\" method=\"get\">\n";
+            print $sock "<input type=\"submit\" name=\"pushcb\" value=\"CB\">\n";
+            print $sock "-&gt; Arg1 -&gt; Arg2 -&gt; Arg3<p>\n";
+            print $sock "<input type=\"submit\" name=\"clear\" value=\"Clear\">\n";
+            print $sock "<input type=\"submit\" name=\"set\" value=\"Set\">\n";
+            print $sock "Quick URL to: $form->{'path'}\n";
+            print $sock "<input type=\"hidden\" name=\"path\" value=\"$form->{'path'}\">\n";
+            print $sock "</form>\n";
+        } else {
+            if ($hide eq 'checked') {
+                print $sock "$ctrl->{'home'} $ctrl->{'HOME'} \n";
+                print $sock "<a href=\"#end\">Jump to end</a>\n";
+                print $sock "<a href=\"/edit.htm?path=$doplpathnow\">Ed</a>\n";
+                print $sock "<a href=\"/view.htm?path=$doplpathnow\">Vw</a>\n";
+                print $sock "<a href=\"/do.htm?path=$doplpathnow\">form</a>\n";
+            }
+            if ($doplpathset) {
+                print $sock "Quick URL is active. Full URL is:<br>\n";
+                $tmp = "/do.htm?do=Do+more&path=$doplpath&arg1=$form->{'arg1'}";
+                print $sock "<a href=\"$tmp\">$tmp</a><br>\n";
+
+                print $sock "<form action=\"/do.htm\" method=\"get\">\n";
+                print $sock "<input type=\"submit\" name=\"set\" value=\"Set\">\n";
+                print $sock "Quick URL to: $form->{'path'}\n";
+                print $sock "<input type=\"hidden\" name=\"path\" value=\"$form->{'path'}\">\n";
+                print $sock "</form>\n";
+            }
+        }
+
 
         print $sock "<a name=\"end\"></a>\n";
 
