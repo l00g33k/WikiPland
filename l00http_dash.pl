ad3b5bc1f2d6d48042f1fa092750d4bf4b0ae381
l00g33k <l00g33k@gmail.com>
2018-05-04 23:52:29 -0700

    dash: ~ for yellow on brown

--- l00http_dash.pl
+++ l00http_dash.pl
@@ -400,6 +400,14 @@ sub l00http_dash_proc {
                 } else {
                     $dsc =~ s/^\^(.+)$/^<strong><font style="color:yellow;background-color:fuchsia">$1<\/font><\/strong>/;
                 }
+                # ~ color fushcia/yellow for do now
+                if ($dsc =~ /^~([^\[\]]+)(\|+[^\[\]]+)$/) {
+                    # special case : "desc | URL" and "desc ||clipboard"
+                    # but not [[URL|desc]]
+                    $dsc = "~<strong><font style=\"color:w;background-color:brown\">$1<\/font><\/strong>$2";
+                } else {
+                    $dsc =~ s/^~(.+)$/~<strong><font style="color:yellow;background-color:brown">$1<\/font><\/strong>/;
+                }
                 if (($cat1 =~ /$catflt/i) && 
                     ($eqlvl == 2)) {
                     # only if match cat1 filter
@@ -615,6 +623,14 @@ sub l00http_dash_proc {
                         $dsc = "^<strong><font style=\"color:yellow;background-color:fuchsia\">$1<\/font><\/strong>$2";
                     } else {
                         $dsc =~ s/^\^(.+)/^<strong><font style="color:yellow;background-color:fuchsia">$1<\/font><\/strong>/;
+                    }
+                    # ~ color fushcia/yellow for do now
+                    if ($dsc =~ /^~([^\[\]]+)(\|+[^\[\]]+)$/) {
+                        # special case : "desc | URL" and "desc ||clipboard"
+                        # but not [[URL|desc]]
+                        $dsc = "~<strong><font style=\"color:w;background-color:brown\">$1<\/font><\/strong>$2";
+                    } else {
+                        $dsc =~ s/^~(.+)$/~<strong><font style="color:yellow;background-color:brown">$1<\/font><\/strong>/;
                     }
                     $tmpbuf .= " - $_";
                 }
