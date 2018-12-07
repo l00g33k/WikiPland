756315bcac51bd78dcf155e8d787f74d77008650
l00g33k <l00g33k@gmail.com>
2018-12-06 17:42:45 -0800

    periodic: allows an expression to be eval, include shell

--- l00http_periodic.pl
+++ l00http_periodic.pl
@@ -5,13 +5,14 @@ use warnings;
 
 # this is a simple template, a good starting point to make your own modules
 
-my ($lastcalled, $perbuf, $percnt, $perltime);
+my ($lastcalled, $perbuf, $percnt, $perltime, $eval);
 my %config = (proc => "l00http_periodic_proc",
               desc => "l00http_periodic_desc",
               perio => "l00http_periodic_perio");
 my $interval = 0, $lastcalled = 0;
 $percnt = 0;
 $perltime = 0;
+$eval = '';
 
 sub l00http_periodic_desc {
     my ($main, $ctrl) = @_;      #$ctrl is a hash, see l00httpd.pl for content definition
@@ -32,7 +33,17 @@ sub l00http_periodic_proc {
         $interval = $form->{"interval"};
     }
 
+    if (defined ($form->{"stop"})) {
+        $interval = 0;
+    }
+
+    if (defined ($form->{"eval"})) {
+        $eval = $form->{"eval"};
+    }
         
+    if (defined ($form->{"paste"})) {
+        $eval = &l00httpd::l00getCB($ctrl);
+    }
 
 
     # Send HTTP and HTML headers
@@ -47,8 +58,14 @@ sub l00http_periodic_proc {
     print $sock "            <td><input type=\"text\" size=\"6\" name=\"interval\" value=\"$interval\"></td>\n";
     print $sock "        </tr>\n";
                                                 
+    print $sock "        <tr>\n";
+    print $sock "        <td><input type=\"submit\" name=\"paste\" value=\"Paste CB\"> eval:</td>\n";
+    print $sock "            <td><input type=\"text\" size=\"10\" name=\"eval\" value=\"$eval\"></td>\n";
+    print $sock "        </tr>\n";
+                                                
     print $sock "    <tr>\n";
-    print $sock "        <td><input type=\"submit\" name=\"submit\" value=\"Submit\"></td>\n";
+    print $sock "        <td><input type=\"submit\" name=\"submit\" value=\"Submit\">\n";
+    print $sock "            <input type=\"submit\" name=\"stop\" value=\"Stop\"></td>\n";
     print $sock "        <td>Note: when phone sleeps, interval may be much longer than specified</td>\n";
     print $sock "    </tr>\n";
 
@@ -57,6 +74,9 @@ sub l00http_periodic_proc {
 
     print $sock "Count: $percnt<br><a href=\"#end\">Jump to end</a>\n";
     print $sock "<pre>$perbuf</pre>\n";
+    
+    print $sock "Windows example: `msg /TIME:1 * The message`<br>\n";
+    print $sock "WikiPland/Android example: TBD<br>\n";
     print $sock "<a name=\"end\"></a>\n";
 
     # send HTML footer and ends
@@ -71,6 +91,9 @@ sub l00http_periodic_perio {
         (($lastcalled == 0) || (time >= ($lastcalled + $interval)))) {
         $lastcalled = time;
 
+        if (length($perbuf) > 10000) {
+            $perbuf = substr($perbuf, 5000, 99999);
+        }
         if ($perltime != 0) {
             $buf = sprintf ("$ctrl->{'now_string'},%d\n", time - $perltime);
             print $buf;
@@ -79,6 +102,7 @@ sub l00http_periodic_perio {
             $perbuf = sprintf ("$ctrl->{'now_string'},$perltime\n");
             print $perbuf;
         }
+        print eval $eval;
         $perltime = time;
         $percnt++;
     }
