b9071ad1f0a15c53709677838ecc645b276094ad
l00g33k <l00g33k@gmail.com>
2019-07-16 00:21:44 -0700

    dash: add view link to top banner

--- l00http_dash.pl
+++ l00http_dash.pl
@@ -302,7 +302,8 @@ sub l00http_dash_proc {
             print $sock "<a href=\"/clip.htm?update=Copy+to+clipboard&clip=:hide+edit+$form->{'path'}%0D\">Path</a>: ";
             print $sock " <a href=\"/ls.htm?path=$pname\">$pname</a>";
             print $sock "<a href=\"/ls.htm?path=$form->{'path'}\">$fname</a> \n";
-            print $sock " <a href=\"/launcher.htm?path=$form->{'path'}\">Launcher</a>\n";
+            print $sock "- <a href=\"/view.htm?path=$form->{'path'}\">view</a> \n";
+            print $sock "- <a href=\"/launcher.htm?path=$form->{'path'}\">Launcher</a>\n";
             print $sock "- <font style=\"color:black;background-color:lime\"><a href=\"#vvv\">vvv</a></font> \n";
             print $sock "- <font style=\"color:black;background-color:LightGray\"><a href=\"/dash.htm?process=Process&path=$form->{'path'}&dash_all=all\#quickcut\">Jump here</a></font> \n";
             print $sock "- <font style=\"color:black;background-color:gold\"><a href=\"#bangbang\">Jump out</a></font> \n";
