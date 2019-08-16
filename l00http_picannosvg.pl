d4704f87d4b052321903bff98ead5a6e9070ce56
l00g33k <l00g33k@gmail.com>
2019-08-16 08:41:25 -0700

    picannosvg: annotating pictures

--- l00http_picannosvg.pl
+++ l00http_picannosvg.pl
@@ -17,8 +17,8 @@ my $scale = 100;
 
 
 my ($mapwd, $mapht);
-$mapwd = 640;
-$mapht = 480;
+$mapwd = 800;
+$mapht = 600;
 
 
 my %config = (proc => "l00http_picannosvg_proc",
@@ -26,19 +26,18 @@ my %config = (proc => "l00http_picannosvg_proc",
 
 
 # converts lon/lat to screen x/y coordinate
-sub ll2xysvg {
+sub annoll2xysvg {
     my ($lonhtm, $lathtm) = @_;
-    my ($pixx, $pixy, $notclip);
-    $notclip = 1;
+    my ($pixx, $pixy);
 
     $pixx = int ($lonhtm * $scale / 100);
     $pixy = int ($mapht * $scale / 100) - int ($lathtm * $scale / 100);
 
-    ($pixx, $pixy, $notclip);
+    ($pixx, $pixy);
 }
 
 # converts screen x/y coordinate to lon/lat
-sub xy2llsvg {
+sub annoxy2llsvg {
     my ($pixx, $pixy) = @_;
     my ($lonhtm, $lathtm);
 
@@ -64,8 +63,9 @@ sub l00http_picannosvg_proc (\%) {
     my $form = $ctrl->{'FORM'};     # dereference FORM data
     my ($pixx, $pixy, $pixx0, $pixy0, $buf, $lonhtm, $lathtm, $xx, $yy);
     my ($lond, $lonm, $lonc, $latd, $latm, $latc);
-    my ($notclip, $coor, $tmp, $svg, $anno);
+    my ($coor, $tmp, $svg, %annos, $xy);
 
+    undef %annos;
 
     if (defined ($form->{'path'})) {
         $path = $form->{'path'};
@@ -73,43 +73,65 @@ sub l00http_picannosvg_proc (\%) {
     }
     # map clicked
     if (defined ($form->{'x'})) {
-        ($lon, $lat) = &xy2llsvg ($form->{'x'}, $form->{'y'});
+        ($lon, $lat) = &annoxy2llsvg ($form->{'x'}, $form->{'y'});
     }
     if (defined ($form->{'scale'})) {
         $scale = $form->{'scale'};
     }
 
-    $anno = '';
-    if (defined($form->{'anno'}) && (length($form->{'anno'}) > 0)) {
-        $anno = $form->{'anno'};
-    }
 
 
-    if (&l00httpd::l00freadOpen($ctrl, $map)) {
-        while ($_ = &l00httpd::l00freadLine($ctrl)) {
-            s/\n//; s/\r//;
-            if (/^IMG_WD_HT/) {
-                ($mapwd, $mapht) = /^IMG_WD_HT=(\d+),(\d+)/;
-                $mapwd = int ($mapwd * $scale / 100);
-                $mapht = int ($mapht * $scale / 100);
-		    }
-        }
-    }
 
     # Send HTTP and HTML headers
     print $sock $ctrl->{'httphead'} . $ctrl->{'htmlhead'} . "<title>picannosvg</title>" . $ctrl->{'htmlhead2'};
 
-    if (defined($form->{'set'})) {
-        print $sock "anno: $anno<br>\n";
+    if (&l00httpd::l00freadOpen($ctrl, $map)) {
+        $buf = '';
+        while ($_ = &l00httpd::l00freadLine($ctrl)) {
+            $buf .= $_;
+            s/\n//; s/\r//;
+            if (/^IMG_WD_HT=/) {
+                ($mapwd, $mapht) = /^IMG_WD_HT=(\d+),(\d+)/;
+                $mapwd = int ($mapwd * $scale / 100);
+                $mapht = int ($mapht * $scale / 100);
+		    }
+            # read annotation
+            if (/^(\d+,\d+): +(.+)$/) {
+                $annos{$1} = $2;
+            }
+        }
+        # write annotation and scale file if missing
+        if (defined($form->{'set'}) &&
+            defined($form->{'anno'}) && 
+            (length($form->{'anno'}) > 0)) {
+            $annos{"$form->{'atx'},$form->{'aty'}"} = $form->{'anno'};
+            $buf .= "$form->{'atx'},$form->{'aty'}: $form->{'anno'}\n";
+            if (&l00httpd::l00fwriteOpen($ctrl, $map)) {
+                &l00httpd::l00fwriteBuf($ctrl, $buf);
+                &l00httpd::l00fwriteClose($ctrl);
+            }
+        }
+    } elsif (&l00httpd::l00fwriteOpen($ctrl, $map)) {
+        &l00httpd::l00fwriteBuf($ctrl, "IMG_WD_HT=$mapwd,$mapht\n");
+        &l00httpd::l00fwriteClose($ctrl);
+        print $sock "Sample <a href=\"/view.htm?path=$map\">$map</a> created as it was missing.  ".
+            "<a href=\"/edit.htm?path=$map\">Edit</a> it for correct image size.<p>\n";
     }
+
 
 
     if (open (IN, "<$path")) {
         close (IN);
-        ($pixx, $pixy, $notclip) = &ll2xysvg ($lon, $lat);
-        if ($notclip) {
+        ($pixx, $pixy) = &annoll2xysvg ($lon, $lat);
+        if (defined($form->{'x'})) {
             print $sock "<div style=\"position: absolute; left:$pixx"."px; top:$pixy"."px;\">\n";
             print $sock "<font color=\"$color\">$marker</font></div>\n";
+        }
+        # display annotations
+        foreach $xy (keys %annos) {
+            ($pixx, $pixy) = split(',', $xy);
+            print $sock "<div style=\"position: absolute; left:$pixx"."px; top:$pixy"."px;\">\n";
+            print $sock "<font color=\"$color\">$annos{$xy}</font></div>\n";
         }
 
         print $sock "<form action=\"/picannosvg.htm\" method=\"get\">\n";
@@ -120,7 +142,7 @@ sub l00http_picannosvg_proc (\%) {
 
 
     if (defined ($form->{'x'})) {
-        print $sock "<br>Clicked pixel (x,y): $form->{'x'},$form->{'y'}\n";
+        print $sock "Clicked pixel (x,y): $form->{'x'},$form->{'y'}\n";
         print $sock "Pic (x,y): ", $form->{'x'} * 100 / $scale,',',$form->{'y'} * 100 / $scale,"<br>\n";
     }
     print $sock "Max px (x,y): $mapwd,$mapht\n";
@@ -136,11 +158,16 @@ sub l00http_picannosvg_proc (\%) {
     print $sock "<form action=\"/picannosvg.htm\" method=\"get\">\n";
     print $sock "<table border=\"1\" cellpadding=\"5\" cellspacing=\"3\">\n";
 
-    print $sock "        <tr>\n";
-    print $sock "            <td>A&#818;nnotation:</td>\n";
-    print $sock "            <td><input type=\"text\" size=\"16\" name=\"anno\" value=\"\" accesskey=\"a\"></td>\n";
-    print $sock "        </tr>\n";
-                                                
+    if (defined ($form->{'x'})) {
+        print $sock "        <tr>\n";
+        print $sock "            <td>A&#818;nnotation:</td>\n";
+        print $sock "            <td><input type=\"text\" size=\"16\" name=\"anno\" value=\"\" accesskey=\"a\">".
+                "<input type=\"hidden\" name=\"atx\" value=\"$form->{'x'}\">\n".
+                "<input type=\"hidden\" name=\"aty\" value=\"$form->{'y'}\">\n".
+                "</td>\n";
+        print $sock "        </tr>\n";
+    }
+                                                    
     print $sock "    <tr>\n";
     print $sock "        <td><input type=\"submit\" name=\"set\" value=\"S&#818;et\" accesskey=\"s\">".
                 "            <input type=\"submit\" name=\"refresh\" value=\"R&#818;efresh\" accesskey=\"r\"></td>\n";
