36b30dd6c43f09ac1185bf9b90ef98cd489a08f4
l00g33k <l00g33k@gmail.com>
2019-08-16 01:13:56 -0700

    picannosvg: wip

--- l00http_picannosvg.pl
+++ l00http_picannosvg.pl
@@ -15,29 +15,10 @@ my $marker = 'X';
 my $color = 'ff0000';
 my $scale = 100;
 
-# long/lat to screen X/Y conversion parameters
-# There are read from .map file
-my $maptlx = 0;
-my $maptly = 0;
-my $mapbrx = 99;
-my $mapbry = 99;
 
-# The scales of the map
-# Initially read from .map file.
-# Modify if 'Force map to fit track
-my $maptllon = -1;
-my $maptllat = 1;
-my $mapbrlon = 1;
-my $mapbrlat = -1;
-
-# The extends of the map
-my $mapextend_tllon = -1;
-my $mapextend_tllat = 1;
-my $mapextend_brlon = 1;
-my $mapextend_brlat = -1;
-
-
-my ($fname, $mapwd, $mapht);
+my ($mapwd, $mapht);
+$mapwd = 640;
+$mapht = 480;
 
 
 my %config = (proc => "l00http_picannosvg_proc",
@@ -50,14 +31,8 @@ sub ll2xysvg {
     my ($pixx, $pixy, $notclip);
     $notclip = 1;
 
-
-    if ($lonhtm > $mapextend_brlon) { $lonhtm = $mapextend_brlon; $notclip = 0; }
-    if ($lonhtm < $mapextend_tllon) { $lonhtm = $mapextend_tllon; $notclip = 0; }
-    if ($lathtm > $mapextend_tllat) { $lathtm = $mapextend_tllat; $notclip = 0; }
-    if ($lathtm < $mapextend_brlat) { $lathtm = $mapextend_brlat; $notclip = 0; }
-    $pixx = $maptlx + int (($lonhtm - $maptllon) / ($mapbrlon - $maptllon) * ($mapbrx - $maptlx) * $scale / 100);
-    $pixy = int ($mapbry * $scale / 100) 
-                    - int (($lathtm - $mapbrlat) / ($maptllat - $mapbrlat) * ($mapbry - $maptly) * $scale / 100);
+    $pixx = int ($lonhtm * $scale / 100);
+    $pixy = int ($mapht * $scale / 100) - int ($lathtm * $scale / 100);
 
     ($pixx, $pixy, $notclip);
 }
@@ -67,12 +42,8 @@ sub xy2llsvg {
     my ($pixx, $pixy) = @_;
     my ($lonhtm, $lathtm);
 
-    $lonhtm = $maptllon + ($pixx - $maptlx) / ($mapbrx - $maptlx) * ($mapbrlon - $maptllon) * 100 / $scale;
-    $lathtm = $maptllat - ($pixy - $maptly) / ($mapbry - $maptly) * ($maptllat - $mapbrlat) * 100 / $scale;
-    #print ",,, $lonhtm = $maptllon + ($pixx - $maptlx) / ($mapbrx - $maptlx) * ($mapbrlon - $maptllon) * 100 / $scale; \n";
-    #print ",,, $lathtm = $maptllat - ($pixy - $maptly) / ($mapbry - $maptly) * ($maptllat - $mapbrlat) * 100 / $scale; \n";
-
-    #print "x,y ($pixx, $pixy) -> lat,long ($lathtm, $lonhtm)\n";
+    $lonhtm = $pixx * 100 / $scale;
+    $lathtm = $mapht - $pixy * 100 / $scale;
 
     ($lonhtm, $lathtm);
 }
@@ -93,12 +64,11 @@ sub l00http_picannosvg_proc (\%) {
     my $form = $ctrl->{'FORM'};     # dereference FORM data
     my ($pixx, $pixy, $pixx0, $pixy0, $buf, $lonhtm, $lathtm, $xx, $yy);
     my ($lond, $lonm, $lonc, $latd, $latm, $latc);
-    my ($notclip, $coor, $tmp, $svg);
+    my ($notclip, $coor, $tmp, $svg, $anno);
 
 
     if (defined ($form->{'path'})) {
         $path = $form->{'path'};
-        ($fname) = $path =~ /\/([^\/]+)$/;
         $map = "$path.txt";
     }
     # map clicked
@@ -109,53 +79,20 @@ sub l00http_picannosvg_proc (\%) {
         $scale = $form->{'scale'};
     }
 
-    if (defined ($form->{'anno'})) {
+    $anno = '';
+    if (defined($form->{'anno'}) && (length($form->{'anno'}) > 0)) {
+        $anno = $form->{'anno'};
     }
 
 
     if (&l00httpd::l00freadOpen($ctrl, $map)) {
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($maptlx) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($maptly) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($maptllon) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($maptllat) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($mapbrx) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($mapbry) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($mapbrlon) = / *([^ ]+) */;
-        $_ = &l00httpd::l00freadLine($ctrl); s/\n//; s/\r//; ($mapbrlat) = / *([^ ]+) */;
-
-        # default
-        $mapwd = int (($mapbrx + 1) * $scale / 100);
-        $mapht = int (($mapbry + 1) * $scale / 100);
-        $_ = &l00httpd::l00freadLine($ctrl); 
-        if (defined($_)) {
+        while ($_ = &l00httpd::l00freadLine($ctrl)) {
             s/\n//; s/\r//;
             if (/^IMG_WD_HT/) {
                 ($mapwd, $mapht) = /^IMG_WD_HT=(\d+),(\d+)/;
                 $mapwd = int ($mapwd * $scale / 100);
                 $mapht = int ($mapht * $scale / 100);
 		    }
-        }
-
-        # compute map extends
-        $mapextend_tllon = $maptllon - ($maptlx)              / ($mapbrx - $maptlx) * ($mapbrlon - $maptllon);
-        $mapextend_tllat = $maptllat + ($maptly)              / ($mapbry - $maptly) * ($maptllat - $mapbrlat);
-        $mapextend_brlon = $mapbrlon + ($mapwd - 1 - $mapbrx) / ($mapbrx - $maptlx) * ($mapbrlon - $maptllon);
-        $mapextend_brlat = $mapbrlat - ($mapht - 1 - $mapbry) / ($mapbry - $maptly) * ($maptllat - $mapbrlat);
-    } else {
-        # .map file doesn't exist, create it.
-        if (&l00httpd::l00fwriteOpen($ctrl, $map)) {
-            &l00httpd::l00fwriteBuf($ctrl, 
-                "10      # line 1: x of top left of graphic map\n".
-                "10      # line 2: y of top left of graphic map\n".
-                "-180    # line 3: longitude of top left of map\n".
-                "90      # line 4: latitude of top left of map\n".
-                "390     # line 5: x of bottom right of graphic map\n".
-                "290     # line 6: y of bottom right of graphic map\n".
-                "180     # line 7: longitude of bottom right of map\n".
-                "-90     # line 8: latitude of bottom right of map\n".
-                "IMG_WD_HT=400,300\n"
-                );
-            &l00httpd::l00fwriteClose($ctrl);
         }
     }
 
@@ -193,13 +130,14 @@ sub l00http_picannosvg_proc (\%) {
     print $sock "<table border=\"1\" cellpadding=\"5\" cellspacing=\"3\">\n";
 
     print $sock "        <tr>\n";
-    print $sock "            <td>Annotation:</td>\n";
-    print $sock "            <td><input type=\"text\" size=\"16\" name=\"anno\" value=\"\"></td>\n";
+    print $sock "            <td>A&#818;nnotation:</td>\n";
+    print $sock "            <td><input type=\"text\" size=\"16\" name=\"anno\" value=\"\" accesskey=\"a\"></td>\n";
     print $sock "        </tr>\n";
                                                 
     print $sock "    <tr>\n";
-    print $sock "        <td><input type=\"submit\" name=\"set\" value=\"Set\"></td>\n";
-    print $sock "        <td>&nbsp;</td>\n";
+    print $sock "        <td><input type=\"submit\" name=\"set\" value=\"S&#818;et\" accesskey=\"s\">".
+                "            <input type=\"submit\" name=\"refresh\" value=\"R&#818;efresh\" accesskey=\"r\"></td>\n";
+    print $sock "        <td>Sc&#818;ale <input type=\"text\" size=\"6\" name=\"scale\" value=\"$scale\" accesskey=\"c\"></td>\n";
     print $sock "    </tr>\n";
 
     print $sock "</table>\n";
