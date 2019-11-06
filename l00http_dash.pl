66f173c82d77b2b78826db354c1c436a111f82c8
l00g33k <l00g33k@gmail.com>
2019-11-06 07:57:19 -0800

    dash: Jump out list all items

--- l00http_dash.pl
+++ l00http_dash.pl
@@ -318,7 +318,7 @@ sub l00http_dash_proc {
             print $sock "- <a href=\"/launcher.htm?path=$form->{'path'}\">Launcher</a>\n";
             print $sock "- <font style=\"color:black;background-color:lime\"><a href=\"#vvv\">vvv</a></font> \n";
             print $sock "- <font style=\"color:black;background-color:LightGray\"><a href=\"/dash.htm?process=Process&path=$form->{'path'}&dash_all=all\#quickcut\">Jump here</a></font> \n";
-            print $sock "- <font style=\"color:black;background-color:gold\"><a href=\"#bangbang\">Jump out</a></font> \n";
+            print $sock "- <font style=\"color:black;background-color:gold\"     ><a href=\"/dash.htm?process=Process&path=$form->{'path'}&dash_all=all\#bangbang\">Jump out</a></font> \n";
         }
     }
     print $sock "<p>\n";
