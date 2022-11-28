6e13dbfba74d9851b6e9baa0515aef38a734e3cf
l00g33k <l00g33k@gmail.com>
2022-11-27 21:18:29 -0800

    dash: hot link

--- l00http_dash.pl
+++ l00http_dash.pl
@@ -323,6 +323,7 @@ sub l00http_dash_proc {
             print $sock "- <a href=\"/edit.htm?path=$form->{'path'}\">Ed</a>/";
             print $sock "<a href=\"/ls.htm?path=$form->{'path'}&exteditor=on\">ext</a>\n";
             print $sock "- <font style=\"color:black;background-color:lime\"><a href=\"#vvv\">vvv</a></font> \n";
+            print $sock "- <font style=\"color:black;background-color:lime\"><a href=\"#hot\">hot</a></font> \n";
             print $sock "- <font style=\"color:black;background-color:LightGray\"><a href=\"/dash.htm?process=Process&path=$form->{'path'}&dash_all=all\#quickcut\">Jump here</a></font> \n";
             print $sock "- <font style=\"color:black;background-color:gold\"     ><a href=\"/dash.htm?process=Process&path=$form->{'path'}&dash_all=all\#bangbang\">Jump out</a></font> \n";
             print $sock "- <font style=\"color:black;background-color:aqua\"     ><a href=\"/dash.htm?process=Process&path=$form->{'path'}&dash_all=all\#descfind\">Desc find</a></font> \n";
